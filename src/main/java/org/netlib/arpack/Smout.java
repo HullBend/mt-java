aa0067ecc9b35e4d87b90b83b0298d4d1a6462d8
HullBend <HullBend@users.noreply.github.com>
2018-11-29 13:16:24 +0100

    sysout stubs to get rid of Util.f77write

---
+++ src/main/java/org/netlib/arpack/Smout.java
@@ -0,0 +1,8 @@
+package org.netlib.arpack;
+
+public final class Smout {
+
+    public static void smout(int i, int j, int k, float af[], int l, int i1, int j1, String s) {
+        // stub
+    }
+}
---
+++ src/main/java/org/netlib/arpack/Svout.java
@@ -0,0 +1,8 @@
+package org.netlib.arpack;
+
+public final class Svout {
+
+    public static void svout(int i, int j, float af[], int k, int l, String s) {
+        // stub
+    }
+}
