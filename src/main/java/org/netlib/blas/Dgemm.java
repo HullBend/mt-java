b91fe4113f3d20aaff038141b5a43a17a6175bae
HullBend <HullBend@users.noreply.github.com>
2018-12-04 00:07:50 +0100

    document Dgemm

--- src/main/java/org/netlib/blas/Dgemm.java
+++ src/main/java/org/netlib/blas/Dgemm.java
@@ -3,206 +3,347 @@ package org.netlib.blas;
 import org.netlib.err.Xerbla;
 
 public final class Dgemm {
-	public static void dgemm(String s, String s1, int i, int j, int k,
-			double d, double ad[], int l, int i1, double ad1[], int j1, int k1,
-			double d1, double ad2[], int l1, int i2) {
-		byte byte0 = 0;
-		int l7 = 0;
-		int i8 = 0;
-		boolean flag4 = false;
-		boolean flag5 = false;
-		flag4 = Lsame.lsame(s, "N");
-		flag5 = Lsame.lsame(s1, "N");
-		if (flag4) {
-			l7 = i;
-		} else {
-			l7 = k;
-		}
-		if (flag5)
-			i8 = k;
-		else
-			i8 = j;
-		byte0 = 0;
-		if ((flag4 ^ true && Lsame.lsame(s, "C") ^ true) && Lsame.lsame(s, "T")
-				^ true)
-			byte0 = 1;
-		else if ((flag5 ^ true && Lsame.lsame(s1, "C") ^ true)
-				&& Lsame.lsame(s1, "T") ^ true)
-			byte0 = 2;
-		else if (i < 0)
-			byte0 = 3;
-		else if (j < 0)
-			byte0 = 4;
-		else if (k < 0)
-			byte0 = 5;
-		else if (i1 < Math.max(1, l7))
-			byte0 = 8;
-		else if (k1 < Math.max(1, i8))
-			byte0 = 10;
-		else if (i2 < Math.max(1, i))
-			byte0 = 13;
-		if (byte0 != 0) {
-			Xerbla.xerbla("DGEMM ", byte0);
-			return;
-		}
-		if (((i == 0) || (j == 0))
-				|| (((d == 0.0D) || (k == 0)) && (d1 == 1.0D)))
-			return;
-		if (d == 0.0D) {
-			if (d1 == 0.0D) {
-				int l4 = 1;
-				for (int j8 = (j - 1) + 1; j8 > 0; j8--) {
-					int j2 = 1;
-					for (int l9 = (i - 1) + 1; l9 > 0; l9--) {
-						ad2[(j2 - 1) + (l4 - 1) * i2 + l1] = 0.0D;
-						j2++;
-					}
+    /**
+    *  Purpose
+    *  =======
+    *
+    *  DGEMM  performs one of the matrix-matrix operations
+    *
+    *     C := alpha*op( A )*op( B ) + beta*C,
+    *
+    *  where  op( X ) is one of
+    *
+    *     op( X ) = X   or   op( X ) = X',
+    *
+    *  alpha and beta are scalars, and A, B and C are matrices, with op( A )
+    *  an m by k matrix,  op( B )  a  k by n matrix and  C an m by n matrix.
+    *
+    *  Arguments
+    *  ==========
+    *
+    *  TRANSA - CHARACTER*1.
+    *           On entry, TRANSA specifies the form of op( A ) to be used in
+    *           the matrix multiplication as follows:
+    *
+    *              TRANSA = 'N' or 'n',  op( A ) = A.
+    *
+    *              TRANSA = 'T' or 't',  op( A ) = A'.
+    *
+    *              TRANSA = 'C' or 'c',  op( A ) = A'.
+    *
+    *           Unchanged on exit.
+    *
+    *  TRANSB - CHARACTER*1.
+    *           On entry, TRANSB specifies the form of op( B ) to be used in
+    *           the matrix multiplication as follows:
+    *
+    *              TRANSB = 'N' or 'n',  op( B ) = B.
+    *
+    *              TRANSB = 'T' or 't',  op( B ) = B'.
+    *
+    *              TRANSB = 'C' or 'c',  op( B ) = B'.
+    *
+    *           Unchanged on exit.
+    *
+    *  M      - INTEGER.
+    *           On entry,  M  specifies  the number  of rows  of the  matrix
+    *           op( A )  and of the  matrix  C.  M  must  be at least  zero.
+    *           Unchanged on exit.
+    *
+    *  N      - INTEGER.
+    *           On entry,  N  specifies the number  of columns of the matrix
+    *           op( B ) and the number of columns of the matrix C. N must be
+    *           at least zero.
+    *           Unchanged on exit.
+    *
+    *  K      - INTEGER.
+    *           On entry,  K  specifies  the number of columns of the matrix
+    *           op( A ) and the number of rows of the matrix op( B ). K must
+    *           be at least  zero.
+    *           Unchanged on exit.
+    *
+    *  ALPHA  - DOUBLE PRECISION.
+    *           On entry, ALPHA specifies the scalar alpha.
+    *           Unchanged on exit.
+    *
+    *  A      - DOUBLE PRECISION array of DIMENSION ( LDA, ka ), where ka is
+    *           k  when  TRANSA = 'N' or 'n',  and is  m  otherwise.
+    *           Before entry with  TRANSA = 'N' or 'n',  the leading  m by k
+    *           part of the array  A  must contain the matrix  A,  otherwise
+    *           the leading  k by m  part of the array  A  must contain  the
+    *           matrix A.
+    *           Unchanged on exit.
+    *
+    *  LDA    - INTEGER.
+    *           On entry, LDA specifies the first dimension of A as declared
+    *           in the calling (sub) program. When  TRANSA = 'N' or 'n' then
+    *           LDA must be at least  max( 1, m ), otherwise  LDA must be at
+    *           least  max( 1, k ).
+    *           Unchanged on exit.
+    *
+    *  B      - DOUBLE PRECISION array of DIMENSION ( LDB, kb ), where kb is
+    *           n  when  TRANSB = 'N' or 'n',  and is  k  otherwise.
+    *           Before entry with  TRANSB = 'N' or 'n',  the leading  k by n
+    *           part of the array  B  must contain the matrix  B,  otherwise
+    *           the leading  n by k  part of the array  B  must contain  the
+    *           matrix B.
+    *           Unchanged on exit.
+    *
+    *  LDB    - INTEGER.
+    *           On entry, LDB specifies the first dimension of B as declared
+    *           in the calling (sub) program. When  TRANSB = 'N' or 'n' then
+    *           LDB must be at least  max( 1, k ), otherwise  LDB must be at
+    *           least  max( 1, n ).
+    *           Unchanged on exit.
+    *
+    *  BETA   - DOUBLE PRECISION.
+    *           On entry,  BETA  specifies the scalar  beta.  When  BETA  is
+    *           supplied as zero then C need not be set on input.
+    *           Unchanged on exit.
+    *
+    *  C      - DOUBLE PRECISION array of DIMENSION ( LDC, n ).
+    *           Before entry, the leading  m by n  part of the array  C must
+    *           contain the matrix  C,  except when  beta  is zero, in which
+    *           case C need not be set on entry.
+    *           On exit, the array  C  is overwritten by the  m by n  matrix
+    *           ( alpha*op( A )*op( B ) + beta*C ).
+    *
+    *  LDC    - INTEGER.
+    *           On entry, LDC specifies the first dimension of C as declared
+    *           in  the  calling  (sub)  program.   LDC  must  be  at  least
+    *           max( 1, m ).
+    *           Unchanged on exit.
+    *
+    *
+    *  Level 3 Blas routine.
+    *
+    *  -- Written on 8-February-1989.
+    *     Jack Dongarra, Argonne National Laboratory.
+    *     Iain Duff, AERE Harwell.
+    *     Jeremy Du Croz, Numerical Algorithms Group Ltd.
+    *     Sven Hammarling, Numerical Algorithms Group Ltd.
+    *
+    *
+    *     .. External Functions ..
+    * </code></pre>
+    *
+    * @param transa
+    * @param transb
+    * @param m
+    * @param n
+    * @param k
+    * @param alpha
+    * @param a
+    * @param _a_offset
+    * @param lda
+    * @param b
+    * @param _b_offset
+    * @param ldb
+    * @param beta
+    * @param c
+    * @param _c_offset
+    * @param ldc
+    */
+    public static void dgemm(String transa, String transb, int m, int n, int k, double alpha, double a[], int _a_offset,
+            int lda, double b[], int _b_offset, int ldb, double beta, double c[], int _c_offset, int ldc) {
 
-					l4++;
-				}
+        byte info = 0;
+        int nrowa = 0;
+        int nrowb = 0;
+        boolean nota = Lsame.lsame(transa, "N");
+        boolean notb = Lsame.lsame(transb, "N");
+        if (nota) {
+            nrowa = m;
+        } else {
+            nrowa = k;
+        }
+        if (notb) {
+            nrowb = k;
+        } else {
+            nrowb = n;
+        }
+        info = 0;
+        if ((!nota && !Lsame.lsame(transa, "C")) && !Lsame.lsame(transa, "T"))
+            info = 1;
+        else if ((!notb && !Lsame.lsame(transb, "C")) && !Lsame.lsame(transb, "T"))
+            info = 2;
+        else if (m < 0)
+            info = 3;
+        else if (n < 0)
+            info = 4;
+        else if (k < 0)
+            info = 5;
+        else if (lda < Math.max(1, nrowa))
+            info = 8;
+        else if (ldb < Math.max(1, nrowb))
+            info = 10;
+        else if (ldc < Math.max(1, m))
+            info = 13;
+        if (info != 0) {
+            Xerbla.xerbla("DGEMM ", info);
+            return;
+        }
+        // Quick return if possible
+        if (((m == 0) || (n == 0)) || (((alpha == 0.0D) || (k == 0)) && (beta == 1.0D))) {
+            return;
+        }
 
-			} else {
-				int i5 = 1;
-				for (int k8 = (j - 1) + 1; k8 > 0; k8--) {
-					int k2 = 1;
-					for (int i10 = (i - 1) + 1; i10 > 0; i10--) {
-						ad2[(k2 - 1) + (i5 - 1) * i2 + l1] = d1
-								* ad2[(k2 - 1) + (i5 - 1) * i2 + l1];
-						k2++;
-					}
+        // And also if alpha == 0
+        if (alpha == 0.0D) {
+            if (beta == 0.0D) {
+                int l4 = 1;
+                for (int j8 = (n - 1) + 1; j8 > 0; j8--) {
+                    int j2 = 1;
+                    for (int l9 = (m - 1) + 1; l9 > 0; l9--) {
+                        c[(j2 - 1) + (l4 - 1) * ldc + _c_offset] = 0.0D;
+                        j2++;
+                    }
 
-					i5++;
-				}
+                    l4++;
+                }
 
-			}
-			return;
-		}
-		if (flag5) {
-			if (flag4) {
-				int j5 = 1;
-				for (int l8 = (j - 1) + 1; l8 > 0; l8--) {
-					if (d1 == 0.0D) {
-						int l2 = 1;
-						for (int j10 = (i - 1) + 1; j10 > 0; j10--) {
-							ad2[(l2 - 1) + (j5 - 1) * i2 + l1] = 0.0D;
-							l2++;
-						}
+            } else {
+                int i5 = 1;
+                for (int k8 = (n - 1) + 1; k8 > 0; k8--) {
+                    int k2 = 1;
+                    for (int i10 = (m - 1) + 1; i10 > 0; i10--) {
+                        c[(k2 - 1) + (i5 - 1) * ldc + _c_offset] = beta * c[(k2 - 1) + (i5 - 1) * ldc + _c_offset];
+                        k2++;
+                    }
 
-					} else if (d1 != 1.0D) {
-						int i3 = 1;
-						for (int k10 = (i - 1) + 1; k10 > 0; k10--) {
-							ad2[(i3 - 1) + (j5 - 1) * i2 + l1] = d1
-									* ad2[(i3 - 1) + (j5 - 1) * i2 + l1];
-							i3++;
-						}
+                    i5++;
+                }
 
-					}
-					int j6 = 1;
-					for (int l10 = (k - 1) + 1; l10 > 0; l10--) {
-						if (ad1[(j6 - 1) + (j5 - 1) * k1 + j1] != 0.0D) {
-							double d3 = d * ad1[(j6 - 1) + (j5 - 1) * k1 + j1];
-							int j3 = 1;
-							for (int j12 = (i - 1) + 1; j12 > 0; j12--) {
-								ad2[(j3 - 1) + (j5 - 1) * i2 + l1] = ad2[(j3 - 1)
-										+ (j5 - 1) * i2 + l1]
-										+ d3 * ad[(j3 - 1) + (j6 - 1) * i1 + l];
-								j3++;
-							}
+            }
+            return;
+        }
 
-						}
-						j6++;
-					}
+        // Start the operations
 
-					j5++;
-				}
+        if (notb) {
+            if (nota) {
+                // Form C := alpha*A*B + beta*C
+                int j5 = 1;
+                for (int l8 = (n - 1) + 1; l8 > 0; l8--) {
+                    if (beta == 0.0D) {
+                        int l2 = 1;
+                        for (int j10 = (m - 1) + 1; j10 > 0; j10--) {
+                            c[(l2 - 1) + (j5 - 1) * ldc + _c_offset] = 0.0D;
+                            l2++;
+                        }
 
-			} else {
-				int k5 = 1;
-				for (int i9 = (j - 1) + 1; i9 > 0; i9--) {
-					int k3 = 1;
-					for (int i11 = (i - 1) + 1; i11 > 0; i11--) {
-						double d4 = 0.0D;
-						int k6 = 1;
-						for (int k12 = (k - 1) + 1; k12 > 0; k12--) {
-							d4 += ad[(k6 - 1) + (k3 - 1) * i1 + l]
-									* ad1[(k6 - 1) + (k5 - 1) * k1 + j1];
-							k6++;
-						}
+                    } else if (beta != 1.0D) {
+                        int i3 = 1;
+                        for (int k10 = (m - 1) + 1; k10 > 0; k10--) {
+                            c[(i3 - 1) + (j5 - 1) * ldc + _c_offset] = beta * c[(i3 - 1) + (j5 - 1) * ldc + _c_offset];
+                            i3++;
+                        }
 
-						if (d1 == 0.0D)
-							ad2[(k3 - 1) + (k5 - 1) * i2 + l1] = d * d4;
-						else
-							ad2[(k3 - 1) + (k5 - 1) * i2 + l1] = d * d4 + d1
-									* ad2[(k3 - 1) + (k5 - 1) * i2 + l1];
-						k3++;
-					}
+                    }
+                    int j6 = 1;
+                    for (int l10 = (k - 1) + 1; l10 > 0; l10--) {
+                        if (b[(j6 - 1) + (j5 - 1) * ldb + _b_offset] != 0.0D) {
+                            double d3 = alpha * b[(j6 - 1) + (j5 - 1) * ldb + _b_offset];
+                            int j3 = 1;
+                            for (int j12 = (m - 1) + 1; j12 > 0; j12--) {
+                                c[(j3 - 1) + (j5 - 1) * ldc + _c_offset] = c[(j3 - 1) + (j5 - 1) * ldc + _c_offset]
+                                        + d3 * a[(j3 - 1) + (j6 - 1) * lda + _a_offset];
+                                j3++;
+                            }
 
-					k5++;
-				}
+                        }
+                        j6++;
+                    }
 
-			}
-		} else if (flag4) {
-			int l5 = 1;
-			for (int j9 = (j - 1) + 1; j9 > 0; j9--) {
-				if (d1 == 0.0D) {
-					int l3 = 1;
-					for (int j11 = (i - 1) + 1; j11 > 0; j11--) {
-						ad2[(l3 - 1) + (l5 - 1) * i2 + l1] = 0.0D;
-						l3++;
-					}
+                    j5++;
+                }
 
-				} else if (d1 != 1.0D) {
-					int i4 = 1;
-					for (int k11 = (i - 1) + 1; k11 > 0; k11--) {
-						ad2[(i4 - 1) + (l5 - 1) * i2 + l1] = d1
-								* ad2[(i4 - 1) + (l5 - 1) * i2 + l1];
-						i4++;
-					}
+            } else {
+                // Form C := alpha*A**T*B + beta*C
+                int k5 = 1;
+                for (int i9 = (n - 1) + 1; i9 > 0; i9--) {
+                    int k3 = 1;
+                    for (int i11 = (m - 1) + 1; i11 > 0; i11--) {
+                        double d4 = 0.0D;
+                        int k6 = 1;
+                        for (int k12 = (k - 1) + 1; k12 > 0; k12--) {
+                            d4 += a[(k6 - 1) + (k3 - 1) * lda + _a_offset] * b[(k6 - 1) + (k5 - 1) * ldb + _b_offset];
+                            k6++;
+                        }
 
-				}
-				int l6 = 1;
-				for (int l11 = (k - 1) + 1; l11 > 0; l11--) {
-					if (ad1[(l5 - 1) + (l6 - 1) * k1 + j1] != 0.0D) {
-						double d5 = d * ad1[(l5 - 1) + (l6 - 1) * k1 + j1];
-						int j4 = 1;
-						for (int l12 = (i - 1) + 1; l12 > 0; l12--) {
-							ad2[(j4 - 1) + (l5 - 1) * i2 + l1] = ad2[(j4 - 1)
-									+ (l5 - 1) * i2 + l1]
-									+ d5 * ad[(j4 - 1) + (l6 - 1) * i1 + l];
-							j4++;
-						}
+                        if (beta == 0.0D)
+                            c[(k3 - 1) + (k5 - 1) * ldc + _c_offset] = alpha * d4;
+                        else
+                            c[(k3 - 1) + (k5 - 1) * ldc + _c_offset] = alpha * d4
+                                    + beta * c[(k3 - 1) + (k5 - 1) * ldc + _c_offset];
+                        k3++;
+                    }
 
-					}
-					l6++;
-				}
+                    k5++;
+                }
 
-				l5++;
-			}
+            }
+        } else if (nota) {
+            // Form C := alpha*A*B**T + beta*C
+            int l5 = 1;
+            for (int j9 = (n - 1) + 1; j9 > 0; j9--) {
+                if (beta == 0.0D) {
+                    int l3 = 1;
+                    for (int j11 = (m - 1) + 1; j11 > 0; j11--) {
+                        c[(l3 - 1) + (l5 - 1) * ldc + _c_offset] = 0.0D;
+                        l3++;
+                    }
 
-		} else {
-			int i6 = 1;
-			for (int k9 = (j - 1) + 1; k9 > 0; k9--) {
-				int k4 = 1;
-				for (int i12 = (i - 1) + 1; i12 > 0; i12--) {
-					double d6 = 0.0D;
-					int i7 = 1;
-					for (int i13 = (k - 1) + 1; i13 > 0; i13--) {
-						d6 += ad[(i7 - 1) + (k4 - 1) * i1 + l]
-								* ad1[(i6 - 1) + (i7 - 1) * k1 + j1];
-						i7++;
-					}
+                } else if (beta != 1.0D) {
+                    int i4 = 1;
+                    for (int k11 = (m - 1) + 1; k11 > 0; k11--) {
+                        c[(i4 - 1) + (l5 - 1) * ldc + _c_offset] = beta * c[(i4 - 1) + (l5 - 1) * ldc + _c_offset];
+                        i4++;
+                    }
 
-					if (d1 == 0.0D)
-						ad2[(k4 - 1) + (i6 - 1) * i2 + l1] = d * d6;
-					else
-						ad2[(k4 - 1) + (i6 - 1) * i2 + l1] = d * d6 + d1
-								* ad2[(k4 - 1) + (i6 - 1) * i2 + l1];
-					k4++;
-				}
+                }
+                int l6 = 1;
+                for (int l11 = (k - 1) + 1; l11 > 0; l11--) {
+                    if (b[(l5 - 1) + (l6 - 1) * ldb + _b_offset] != 0.0D) {
+                        double d5 = alpha * b[(l5 - 1) + (l6 - 1) * ldb + _b_offset];
+                        int j4 = 1;
+                        for (int l12 = (m - 1) + 1; l12 > 0; l12--) {
+                            c[(j4 - 1) + (l5 - 1) * ldc + _c_offset] = c[(j4 - 1) + (l5 - 1) * ldc + _c_offset]
+                                    + d5 * a[(j4 - 1) + (l6 - 1) * lda + _a_offset];
+                            j4++;
+                        }
 
-				i6++;
-			}
+                    }
+                    l6++;
+                }
 
-		}
-	}
+                l5++;
+            }
+
+        } else {
+            // Form C := alpha*A**T*B**T + beta*C
+            int i6 = 1;
+            for (int k9 = (n - 1) + 1; k9 > 0; k9--) {
+                int k4 = 1;
+                for (int i12 = (m - 1) + 1; i12 > 0; i12--) {
+                    double d6 = 0.0D;
+                    int i7 = 1;
+                    for (int i13 = (k - 1) + 1; i13 > 0; i13--) {
+                        d6 += a[(i7 - 1) + (k4 - 1) * lda + _a_offset] * b[(i6 - 1) + (i7 - 1) * ldb + _b_offset];
+                        i7++;
+                    }
+
+                    if (beta == 0.0D)
+                        c[(k4 - 1) + (i6 - 1) * ldc + _c_offset] = alpha * d6;
+                    else
+                        c[(k4 - 1) + (i6 - 1) * ldc + _c_offset] = alpha * d6
+                                + beta * c[(k4 - 1) + (i6 - 1) * ldc + _c_offset];
+                    k4++;
+                }
+
+                i6++;
+            }
+
+        }
+    }
 }
