efc8641878f25e0b440a2e2ac3987e73bf97b18a
HullBend <HullBend@users.noreply.github.com>
2018-12-04 22:10:41 +0100

    remove some unused single precision ARPACK classes

--- src/main/java/org/netlib/arpack/Smout.java
+++
@@ -1,8 +0,0 @@
-package org.netlib.arpack;
-
-public final class Smout {
-
-    public static void smout(int i, int j, int k, float af[], int l, int i1, int j1, String s) {
-        // stub
-    }
-}
--- src/main/java/org/netlib/arpack/Svout.java
+++
@@ -1,8 +0,0 @@
-package org.netlib.arpack;
-
-public final class Svout {
-
-    public static void svout(int i, int j, float af[], int k, int l, String s) {
-        // stub
-    }
-}
