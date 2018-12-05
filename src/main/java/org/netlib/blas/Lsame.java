a348004d7f6b3eb1b77db8d4a9ef461d2f304034
HullBend <HullBend@users.noreply.github.com>
2018-12-05 23:45:45 +0100

    fix serious Lsame bug

--- src/main/java/org/netlib/blas/Lsame.java
+++ src/main/java/org/netlib/blas/Lsame.java
@@ -2,6 +2,6 @@ package org.netlib.blas;
 
 public final class Lsame {
     public static boolean lsame(String s, String s1) {
-        return s1.equalsIgnoreCase(s);
+    	return s1.substring(0, 1).equalsIgnoreCase(s.substring(0, 1));
     }
 }
