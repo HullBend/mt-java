e671f300748a8d3731305e1f510229c2b47d7494
HullBend <HullBend@users.noreply.github.com>
2018-12-10 10:23:31 +0100

    parameter names

--- src/main/java/org/netlib/blas/Daxpy.java
+++ src/main/java/org/netlib/blas/Daxpy.java
@@ -1,55 +1,55 @@
 package org.netlib.blas;
 
 public final class Daxpy {
-	public static void daxpy(int i, double d, double ad[], int j, int k,
-			double ad1[], int l, int i1) {
-		int j1 = 0;
-		int i2 = 0;
-		int j2 = 0;
-		if (i <= 0)
-			return;
-		if (d == 0.0D)
-			return;
-		if ((k != 1) || (i1 != 1)) {
-			int k1 = 1;
-			int l1 = 1;
-			if (k < 0)
-				k1 = (-i + 1) * k + 1;
-			if (i1 < 0)
-				l1 = (-i + 1) * i1 + 1;
-			j1 = 1;
-			for (int k2 = (i - 1) + 1; k2 > 0; k2--) {
-				ad1[(l1 - 1) + l] = ad1[(l1 - 1) + l] + d * ad[(k1 - 1) + j];
-				k1 += k;
-				l1 += i1;
-				j1++;
-			}
 
-			return;
-		}
-		i2 = i % 4;
-		if (i2 != 0) {
-			j1 = 1;
-			for (int l2 = (i2 - 1) + 1; l2 > 0; l2--) {
-				ad1[(j1 - 1) + l] = ad1[(j1 - 1) + l] + d * ad[(j1 - 1) + j];
-				j1++;
-			}
+    public static void daxpy(int n, double da, double[] dx, int _dx_offset, int incx,
+            double[] dy, int _dy_offset, int incy) {
 
-			if (i < 4)
-				return;
-		}
-		j2 = i2 + 1;
-		j1 = j2;
-		for (int i3 = ((i - j2) + 4) / 4; i3 > 0; i3--) {
-			ad1[(j1 - 1) + l] = ad1[(j1 - 1) + l] + d * ad[(j1 - 1) + j];
-			ad1[((j1 + 1) - 1) + l] = ad1[((j1 + 1) - 1) + l] + d
-					* ad[((j1 + 1) - 1) + j];
-			ad1[((j1 + 2) - 1) + l] = ad1[((j1 + 2) - 1) + l] + d
-					* ad[((j1 + 2) - 1) + j];
-			ad1[((j1 + 3) - 1) + l] = ad1[((j1 + 3) - 1) + l] + d
-					* ad[((j1 + 3) - 1) + j];
-			j1 += 4;
-		}
+        if (n <= 0 || da == 0.0) {
+            return;
+        }
 
-	}
+        int j1 = 0;
+
+        if ((incx != 1) || (incy != 1)) {
+            int k1 = 1;
+            int l1 = 1;
+            if (incx < 0) {
+                k1 = (-n + 1) * incx + 1;
+            }
+            if (incy < 0) {
+                l1 = (-n + 1) * incy + 1;
+            }
+            j1 = 1;
+            for (int k2 = (n - 1) + 1; k2 > 0; k2--) {
+                dy[(l1 - 1) + _dy_offset] = dy[(l1 - 1) + _dy_offset] + da * dx[(k1 - 1) + _dx_offset];
+                k1 += incx;
+                l1 += incy;
+                j1++;
+            }
+
+            return;
+        }
+        int i2 = n % 4;
+        if (i2 != 0) {
+            j1 = 1;
+            for (int l2 = (i2 - 1) + 1; l2 > 0; l2--) {
+                dy[(j1 - 1) + _dy_offset] = dy[(j1 - 1) + _dy_offset] + da * dx[(j1 - 1) + _dx_offset];
+                j1++;
+            }
+
+            if (n < 4) {
+                return;
+            }
+        }
+        int j2 = i2 + 1;
+        j1 = j2;
+        for (int i3 = ((n - j2) + 4) / 4; i3 > 0; i3--) {
+            dy[j1 - 1 + _dy_offset] = dy[j1 - 1 + _dy_offset] + da * dx[j1 - 1 + _dx_offset];
+            dy[j1 + _dy_offset    ] = dy[j1     + _dy_offset] + da * dx[j1     + _dx_offset];
+            dy[j1 + 1 + _dy_offset] = dy[j1 + 1 + _dy_offset] + da * dx[j1 + 1 + _dx_offset];
+            dy[j1 + 2 + _dy_offset] = dy[j1 + 2 + _dy_offset] + da * dx[j1 + 2 + _dx_offset];
+            j1 += 4;
+        }
+    }
 }
