9df8b313252b276a3c071212e12afdcdb3340ed0
HullBend <HullBend@users.noreply.github.com>
2018-12-13 19:52:32 +0100

    minor Dger cosmetics

--- src/main/java/org/netlib/blas/Dger.java
+++ src/main/java/org/netlib/blas/Dger.java
@@ -3,8 +3,10 @@ package org.netlib.blas;
 import org.netlib.err.Xerbla;
 
 public final class Dger {
+
 	public static void dger(int i, int j, double d, double ad[], int k, int l,
 			double ad1[], int i1, int j1, double ad2[], int k1, int l1) {
+
 		byte byte0 = 0;
 		int j3 = 0;
 		byte0 = 0;
@@ -22,22 +24,21 @@ public final class Dger {
 			Xerbla.xerbla("DGER  ", byte0);
 			return;
 		}
-		if (((i == 0) || (j == 0)) || (d == 0.0D))
+		if ((i == 0 || j == 0) || d == 0.0)
 			return;
-		if (j1 > 0)
+		if (j1 > 0) {
 			j3 = 1;
-		else
+		} else {
 			j3 = 1 - (j - 1) * j1;
+		}
 		if (l == 1) {
 			int l2 = 1;
-			for (int l3 = (j - 1) + 1; l3 > 0; l3--) {
-				if (ad1[(j3 - 1) + i1] != 0.0D) {
-					double d2 = d * ad1[(j3 - 1) + i1];
+			for (int l3 = j; l3 > 0; l3--) {
+				if (ad1[j3 - 1 + i1] != 0.0) {
+					double d2 = d * ad1[j3 - 1 + i1];
 					int i2 = 1;
-					for (int j4 = (i - 1) + 1; j4 > 0; j4--) {
-						ad2[(i2 - 1) + (l2 - 1) * l1 + k1] = ad2[(i2 - 1)
-								+ (l2 - 1) * l1 + k1]
-								+ ad[(i2 - 1) + k] * d2;
+					for (int j4 = i; j4 > 0; j4--) {
+						ad2[i2 - 1 + (l2 - 1) * l1 + k1] = ad2[i2 - 1 + (l2 - 1) * l1 + k1] + ad[i2 - 1 + k] * d2;
 						i2++;
 					}
 
@@ -48,20 +49,19 @@ public final class Dger {
 
 		} else {
 			int k3;
-			if (l > 0)
+			if (l > 0) {
 				k3 = 1;
-			else
+			} else {
 				k3 = 1 - (i - 1) * l;
+			}
 			int i3 = 1;
-			for (int i4 = (j - 1) + 1; i4 > 0; i4--) {
-				if (ad1[(j3 - 1) + i1] != 0.0D) {
-					double d3 = d * ad1[(j3 - 1) + i1];
+			for (int i4 = j; i4 > 0; i4--) {
+				if (ad1[j3 - 1 + i1] != 0.0) {
+					double d3 = d * ad1[j3 - 1 + i1];
 					int k2 = k3;
 					int j2 = 1;
-					for (int k4 = (i - 1) + 1; k4 > 0; k4--) {
-						ad2[(j2 - 1) + (i3 - 1) * l1 + k1] = ad2[(j2 - 1)
-								+ (i3 - 1) * l1 + k1]
-								+ ad[(k2 - 1) + k] * d3;
+					for (int k4 = i; k4 > 0; k4--) {
+						ad2[j2 - 1 + (i3 - 1) * l1 + k1] = ad2[j2 - 1 + (i3 - 1) * l1 + k1] + ad[k2 - 1 + k] * d3;
 						k2 += l;
 						j2++;
 					}
