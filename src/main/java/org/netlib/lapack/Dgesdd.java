c60c2ccb024add7c2abd7553d69d3ef44e0f4539
HullBend <HullBend@users.noreply.github.com>
2018-12-16 18:31:52 +0100

    minor cleanup

--- src/main/java/org/netlib/lapack/Dgesdd.java
+++ src/main/java/org/netlib/lapack/Dgesdd.java
@@ -30,8 +30,8 @@ public final class Dgesdd
         double d1 = 0.0D;
         double d2 = 0.0D;
         double d3 = 0.0D;
-        int ai1[] = new int[1];
-        double ad5[] = new double[1];
+        int[] ai1 = new int[1];
+        double[] ad5 = new double[1];
         intw.val = 0;
         k18 = Math.min(i, j);
         flag1 = Lsame.lsame(s, "A");
@@ -40,7 +40,7 @@ public final class Dgesdd
         flag4 = Lsame.lsame(s, "O");
         flag3 = Lsame.lsame(s, "N");
         flag = k2 == -1;
-        if((((flag1 || flag5) || flag4) || flag3) ^ true)
+        if(!(((flag1 || flag5) || flag4) || flag3))
             intw.val = -1;
         else
         if(i < 0)
@@ -63,7 +63,7 @@ public final class Dgesdd
             j18 = 1;
             if((i >= j) && (k18 > 0))
             {
-                i19 = (int)(((double)k18 * 11D) / 6D);
+                i19 = (int)(((double)k18 * 11.0) / 6.0);
                 if(flag3)
                     i3 = 7 * j;
                 else
@@ -144,7 +144,7 @@ public final class Dgesdd
             } else
             if(k18 > 0)
             {
-                i19 = (int)(((double)k18 * 11D) / 6D);
+                i19 = (int)(((double)k18 * 11.0) / 6.0);
                 if(flag3)
                     i3 = 7 * i;
                 else
@@ -224,7 +224,7 @@ public final class Dgesdd
                 }
             }
             j18 = Math.max(j18, l18);
-            ad4[(1 - 1) + j2] = j18;
+            ad4[j2] = j18;
             if((k2 < l18) && flag ^ true)
                 intw.val = -12;
         }
@@ -239,10 +239,10 @@ public final class Dgesdd
             return;
         d2 = Dlamch.dlamch("P");
         d3 = Math.sqrt(Dlamch.dlamch("S")) / d2;
-        d1 = 1.0D / d3;
+        d1 = 1.0 / d3;
         d = Dlange.dlange("M", i, j, ad, k, l, ad5, 0);
         flag9 = false;
-        if((d > 0.0D) && (d < d3))
+        if((d > 0.0) && (d < d3))
         {
             flag9 = true;
             Dlascl.dlascl("G", 0, 0, d, d3, i, j, ad, k, l, intw1);
@@ -261,7 +261,7 @@ public final class Dgesdd
                     int i8 = 1;
                     int j19 = i8 + j;
                     Dgeqrf.dgeqrf(i, j, ad, k, l, ad4, (i8 - 1) + j2, ad4, (j19 - 1) + j2, (k2 - j19) + 1, intw1);
-                    Dlaset.dlaset("L", j - 1, j - 1, 0.0D, 0.0D, ad, (2 - 1) + (1 - 1) * l + k, l);
+                    Dlaset.dlaset("L", j - 1, j - 1, 0.0, 0.0, ad, 1 + k, l);
                     int i5 = 1;
                     int k12 = i5 + j;
                     int i10 = k12 + j;
@@ -281,7 +281,7 @@ public final class Dgesdd
                     int k19 = j8 + j;
                     Dgeqrf.dgeqrf(i, j, ad, k, l, ad4, (j8 - 1) + j2, ad4, (k19 - 1) + j2, (k2 - k19) + 1, intw1);
                     Dlacpy.dlacpy("U", j, j, ad, k, l, ad4, (l7 - 1) + j2, k17);
-                    Dlaset.dlaset("L", j - 1, j - 1, 0.0D, 0.0D, ad4, ((l7 + 1) - 1) + j2, k17);
+                    Dlaset.dlaset("L", j - 1, j - 1, 0.0, 0.0, ad4, ((l7 + 1) - 1) + j2, k17);
                     Dorgqr.dorgqr(i, j, j, ad, k, l, ad4, (j8 - 1) + j2, ad4, (k19 - 1) + j2, (k2 - k19) + 1, intw1);
                     int j5 = j8;
                     int l12 = j5 + j;
@@ -297,8 +297,8 @@ public final class Dgesdd
                     for(int j24 = ((i - 1) + k17) / k17; j24 > 0; j24--)
                     {
                         l3 = Math.min((i - i4) + 1, k17);
-                        Dgemm.dgemm("N", "N", l3, j, j, 1.0D, ad, (i4 - 1) + (1 - 1) * l + k, l, ad4, (i15 - 1) + j2, j, 0.0D, ad4, (l7 - 1) + j2, k17);
-                        Dlacpy.dlacpy("F", l3, j, ad4, (l7 - 1) + j2, k17, ad, (i4 - 1) + (1 - 1) * l + k, l);
+                        Dgemm.dgemm("N", "N", l3, j, j, 1.0, ad, (i4 - 1) + k, l, ad4, (i15 - 1) + j2, j, 0.0, ad4, (l7 - 1) + j2, k17);
+                        Dlacpy.dlacpy("F", l3, j, ad4, (l7 - 1) + j2, k17, ad, (i4 - 1) + k, l);
                         i4 += k17;
                     }
 
@@ -311,7 +311,7 @@ public final class Dgesdd
                     int l19 = k8 + j;
                     Dgeqrf.dgeqrf(i, j, ad, k, l, ad4, (k8 - 1) + j2, ad4, (l19 - 1) + j2, (k2 - l19) + 1, intw1);
                     Dlacpy.dlacpy("U", j, j, ad, k, l, ad4, (l7 - 1) + j2, k17);
-                    Dlaset.dlaset("L", j - 1, j - 1, 0.0D, 0.0D, ad4, ((l7 + 1) - 1) + j2, k17);
+                    Dlaset.dlaset("L", j - 1, j - 1, 0.0, 0.0, ad4, ((l7 + 1) - 1) + j2, k17);
                     Dorgqr.dorgqr(i, j, j, ad, k, l, ad4, (k8 - 1) + j2, ad4, (l19 - 1) + j2, (k2 - l19) + 1, intw1);
                     int k5 = k8;
                     int i13 = k5 + j;
@@ -322,7 +322,7 @@ public final class Dgesdd
                     Dormbr.dormbr("Q", "L", "N", j, j, j, ad4, (l7 - 1) + j2, k17, ad4, (i13 - 1) + j2, ad2, j1, k1, ad4, (l19 - 1) + j2, (k2 - l19) + 1, intw1);
                     Dormbr.dormbr("P", "R", "T", j, j, j, ad4, (l7 - 1) + j2, k17, ad4, (k10 - 1) + j2, ad3, l1, i2, ad4, (l19 - 1) + j2, (k2 - l19) + 1, intw1);
                     Dlacpy.dlacpy("F", j, j, ad2, j1, k1, ad4, (l7 - 1) + j2, k17);
-                    Dgemm.dgemm("N", "N", i, j, j, 1.0D, ad, k, l, ad4, (l7 - 1) + j2, k17, 0.0D, ad2, j1, k1);
+                    Dgemm.dgemm("N", "N", i, j, j, 1.0, ad, k, l, ad4, (l7 - 1) + j2, k17, 0.0, ad2, j1, k1);
                 } else
                 if(flag1)
                 {
@@ -333,7 +333,7 @@ public final class Dgesdd
                     Dgeqrf.dgeqrf(i, j, ad, k, l, ad4, (l8 - 1) + j2, ad4, (i20 - 1) + j2, (k2 - i20) + 1, intw1);
                     Dlacpy.dlacpy("L", i, j, ad, k, l, ad2, j1, k1);
                     Dorgqr.dorgqr(i, i, j, ad2, j1, k1, ad4, (l8 - 1) + j2, ad4, (i20 - 1) + j2, (k2 - i20) + 1, intw1);
-                    Dlaset.dlaset("L", j - 1, j - 1, 0.0D, 0.0D, ad, (2 - 1) + (1 - 1) * l + k, l);
+                    Dlaset.dlaset("L", j - 1, j - 1, 0.0, 0.0, ad, 1 + k, l);
                     int l5 = l8;
                     int j13 = l5 + j;
                     int l10 = j13 + j;
@@ -342,7 +342,7 @@ public final class Dgesdd
                     Dbdsdc.dbdsdc("U", "I", j, ad1, i1, ad4, (l5 - 1) + j2, ad4, (j15 - 1) + j2, j, ad3, l1, i2, ad5, 0, ai1, 0, ad4, (i20 - 1) + j2, ai, l2, intw);
                     Dormbr.dormbr("Q", "L", "N", j, j, j, ad, k, l, ad4, (j13 - 1) + j2, ad4, (j15 - 1) + j2, l17, ad4, (i20 - 1) + j2, (k2 - i20) + 1, intw1);
                     Dormbr.dormbr("P", "R", "T", j, j, j, ad, k, l, ad4, (l10 - 1) + j2, ad3, l1, i2, ad4, (i20 - 1) + j2, (k2 - i20) + 1, intw1);
-                    Dgemm.dgemm("N", "N", i, j, j, 1.0D, ad2, j1, k1, ad4, (j15 - 1) + j2, l17, 0.0D, ad, k, l);
+                    Dgemm.dgemm("N", "N", i, j, j, 1.0, ad2, j1, k1, ad4, (j15 - 1) + j2, l17, 0.0, ad, k, l);
                     Dlacpy.dlacpy("F", i, j, ad, k, l, ad2, j1, k1);
                 }
             } else
@@ -363,7 +363,7 @@ public final class Dgesdd
                     {
                         i18 = i;
                         j20 = k15 + i18 * j;
-                        Dlaset.dlaset("F", i, j, 0.0D, 0.0D, ad4, (k15 - 1) + j2, i18);
+                        Dlaset.dlaset("F", i, j, 0.0, 0.0, ad4, (k15 - 1) + j2, i18);
                     } else
                     {
                         i18 = j;
@@ -385,8 +385,8 @@ public final class Dgesdd
                         for(int k24 = ((i - 1) + k17) / k17; k24 > 0; k24--)
                         {
                             l3 = Math.min((i - j4) + 1, k17);
-                            Dgemm.dgemm("N", "N", l3, j, j, 1.0D, ad, (j4 - 1) + (1 - 1) * l + k, l, ad4, (k15 - 1) + j2, i18, 0.0D, ad4, (l7 - 1) + j2, k17);
-                            Dlacpy.dlacpy("F", l3, j, ad4, (l7 - 1) + j2, k17, ad, (j4 - 1) + (1 - 1) * l + k, l);
+                            Dgemm.dgemm("N", "N", l3, j, j, 1.0, ad, (j4 - 1) + k, l, ad4, (k15 - 1) + j2, i18, 0.0, ad4, (l7 - 1) + j2, k17);
+                            Dlacpy.dlacpy("F", l3, j, ad4, (l7 - 1) + j2, k17, ad, (j4 - 1) + k, l);
                             j4 += k17;
                         }
 
@@ -394,17 +394,17 @@ public final class Dgesdd
                 } else
                 if(flag5)
                 {
-                    Dlaset.dlaset("F", i, j, 0.0D, 0.0D, ad2, j1, k1);
+                    Dlaset.dlaset("F", i, j, 0.0, 0.0, ad2, j1, k1);
                     Dbdsdc.dbdsdc("U", "I", j, ad1, i1, ad4, (i6 - 1) + j2, ad2, j1, k1, ad3, l1, i2, ad5, 0, ai1, 0, ad4, (j20 - 1) + j2, ai, l2, intw);
                     Dormbr.dormbr("Q", "L", "N", i, j, j, ad, k, l, ad4, (k13 - 1) + j2, ad2, j1, k1, ad4, (j20 - 1) + j2, (k2 - j20) + 1, intw1);
                     Dormbr.dormbr("P", "R", "T", j, j, j, ad, k, l, ad4, (i11 - 1) + j2, ad3, l1, i2, ad4, (j20 - 1) + j2, (k2 - j20) + 1, intw1);
                 } else
                 if(flag1)
                 {
-                    Dlaset.dlaset("F", i, i, 0.0D, 0.0D, ad2, j1, k1);
+                    Dlaset.dlaset("F", i, i, 0.0, 0.0, ad2, j1, k1);
                     Dbdsdc.dbdsdc("U", "I", j, ad1, i1, ad4, (i6 - 1) + j2, ad2, j1, k1, ad3, l1, i2, ad5, 0, ai1, 0, ad4, (j20 - 1) + j2, ai, l2, intw);
                     if(i > j)
-                        Dlaset.dlaset("F", i - j, i - j, 0.0D, 1.0D, ad2, ((j + 1) - 1) + ((j + 1) - 1) * k1 + j1, k1);
+                        Dlaset.dlaset("F", i - j, i - j, 0.0, 1.0, ad2, ((j + 1) - 1) + ((j + 1) - 1) * k1 + j1, k1);
                     Dormbr.dormbr("Q", "L", "N", i, i, j, ad, k, l, ad4, (k13 - 1) + j2, ad2, j1, k1, ad4, (j20 - 1) + j2, (k2 - j20) + 1, intw1);
                     Dormbr.dormbr("P", "R", "T", j, j, i, ad, k, l, ad4, (i11 - 1) + j2, ad3, l1, i2, ad4, (j20 - 1) + j2, (k2 - j20) + 1, intw1);
                 }
@@ -417,7 +417,7 @@ public final class Dgesdd
                 int i9 = 1;
                 int k20 = i9 + i;
                 Dgelqf.dgelqf(i, j, ad, k, l, ad4, (i9 - 1) + j2, ad4, (k20 - 1) + j2, (k2 - k20) + 1, intw1);
-                Dlaset.dlaset("U", i - 1, i - 1, 0.0D, 0.0D, ad, (1 - 1) + (2 - 1) * l + k, l);
+                Dlaset.dlaset("U", i - 1, i - 1, 0.0, 0.0, ad, l + k, l);
                 int j6 = 1;
                 int l13 = j6 + i;
                 int j11 = l13 + i;
@@ -444,7 +444,7 @@ public final class Dgesdd
                 int l20 = j9 + i;
                 Dgelqf.dgelqf(i, j, ad, k, l, ad4, (j9 - 1) + j2, ad4, (l20 - 1) + j2, (k2 - l20) + 1, intw1);
                 Dlacpy.dlacpy("L", i, i, ad, k, l, ad4, (k7 - 1) + j2, i17);
-                Dlaset.dlaset("U", i - 1, i - 1, 0.0D, 0.0D, ad4, ((k7 + i17) - 1) + j2, i17);
+                Dlaset.dlaset("U", i - 1, i - 1, 0.0, 0.0, ad4, ((k7 + i17) - 1) + j2, i17);
                 Dorglq.dorglq(i, j, i, ad, k, l, ad4, (j9 - 1) + j2, ad4, (l20 - 1) + j2, (k2 - l20) + 1, intw1);
                 int k6 = j9;
                 int i14 = k6 + i;
@@ -458,8 +458,8 @@ public final class Dgesdd
                 for(int l24 = ((j - 1) + l3) / l3; l24 > 0; l24--)
                 {
                     int j3 = Math.min((j - k4) + 1, l3);
-                    Dgemm.dgemm("N", "N", i, j3, i, 1.0D, ad4, (l15 - 1) + j2, i, ad, (1 - 1) + (k4 - 1) * l + k, l, 0.0D, ad4, (k7 - 1) + j2, i17);
-                    Dlacpy.dlacpy("F", i, j3, ad4, (k7 - 1) + j2, i17, ad, (1 - 1) + (k4 - 1) * l + k, l);
+                    Dgemm.dgemm("N", "N", i, j3, i, 1.0, ad4, (l15 - 1) + j2, i, ad, (k4 - 1) * l + k, l, 0.0, ad4, (k7 - 1) + j2, i17);
+                    Dlacpy.dlacpy("F", i, j3, ad4, (k7 - 1) + j2, i17, ad, (k4 - 1) * l + k, l);
                     k4 += l3;
                 }
 
@@ -472,7 +472,7 @@ public final class Dgesdd
                 int i21 = k9 + i;
                 Dgelqf.dgelqf(i, j, ad, k, l, ad4, (k9 - 1) + j2, ad4, (i21 - 1) + j2, (k2 - i21) + 1, intw1);
                 Dlacpy.dlacpy("L", i, i, ad, k, l, ad4, (k7 - 1) + j2, j17);
-                Dlaset.dlaset("U", i - 1, i - 1, 0.0D, 0.0D, ad4, ((k7 + j17) - 1) + j2, j17);
+                Dlaset.dlaset("U", i - 1, i - 1, 0.0, 0.0, ad4, ((k7 + j17) - 1) + j2, j17);
                 Dorglq.dorglq(i, j, i, ad, k, l, ad4, (k9 - 1) + j2, ad4, (i21 - 1) + j2, (k2 - i21) + 1, intw1);
                 int l6 = k9;
                 int j14 = l6 + i;
@@ -483,7 +483,7 @@ public final class Dgesdd
                 Dormbr.dormbr("Q", "L", "N", i, i, i, ad4, (k7 - 1) + j2, j17, ad4, (j14 - 1) + j2, ad2, j1, k1, ad4, (i21 - 1) + j2, (k2 - i21) + 1, intw1);
                 Dormbr.dormbr("P", "R", "T", i, i, i, ad4, (k7 - 1) + j2, j17, ad4, (l11 - 1) + j2, ad3, l1, i2, ad4, (i21 - 1) + j2, (k2 - i21) + 1, intw1);
                 Dlacpy.dlacpy("F", i, i, ad3, l1, i2, ad4, (k7 - 1) + j2, j17);
-                Dgemm.dgemm("N", "N", i, j, i, 1.0D, ad4, (k7 - 1) + j2, j17, ad, k, l, 0.0D, ad3, l1, i2);
+                Dgemm.dgemm("N", "N", i, j, i, 1.0, ad4, (k7 - 1) + j2, j17, ad, k, l, 0.0, ad3, l1, i2);
             } else
             if(flag1)
             {
@@ -494,7 +494,7 @@ public final class Dgesdd
                 Dgelqf.dgelqf(i, j, ad, k, l, ad4, (l9 - 1) + j2, ad4, (j21 - 1) + j2, (k2 - j21) + 1, intw1);
                 Dlacpy.dlacpy("U", i, j, ad, k, l, ad3, l1, i2);
                 Dorglq.dorglq(j, j, i, ad3, l1, i2, ad4, (l9 - 1) + j2, ad4, (j21 - 1) + j2, (k2 - j21) + 1, intw1);
-                Dlaset.dlaset("U", i - 1, i - 1, 0.0D, 0.0D, ad, (1 - 1) + (2 - 1) * l + k, l);
+                Dlaset.dlaset("U", i - 1, i - 1, 0.0, 0.0, ad, l + k, l);
                 int i7 = l9;
                 int k14 = i7 + i;
                 int i12 = k14 + i;
@@ -503,7 +503,7 @@ public final class Dgesdd
                 Dbdsdc.dbdsdc("U", "I", i, ad1, i1, ad4, (i7 - 1) + j2, ad2, j1, k1, ad4, (i16 - 1) + j2, k16, ad5, 0, ai1, 0, ad4, (j21 - 1) + j2, ai, l2, intw);
                 Dormbr.dormbr("Q", "L", "N", i, i, i, ad, k, l, ad4, (k14 - 1) + j2, ad2, j1, k1, ad4, (j21 - 1) + j2, (k2 - j21) + 1, intw1);
                 Dormbr.dormbr("P", "R", "T", i, i, i, ad, k, l, ad4, (i12 - 1) + j2, ad4, (i16 - 1) + j2, k16, ad4, (j21 - 1) + j2, (k2 - j21) + 1, intw1);
-                Dgemm.dgemm("N", "N", i, j, i, 1.0D, ad4, (i16 - 1) + j2, k16, ad3, l1, i2, 0.0D, ad, k, l);
+                Dgemm.dgemm("N", "N", i, j, i, 1.0, ad4, (i16 - 1) + j2, k16, ad3, l1, i2, 0.0, ad, k, l);
                 Dlacpy.dlacpy("F", i, j, ad, k, l, ad3, l1, i2);
             }
         } else
@@ -522,7 +522,7 @@ public final class Dgesdd
                 int j16 = k21;
                 if(k2 >= i * j + 3 * i + i3)
                 {
-                    Dlaset.dlaset("F", i, j, 0.0D, 0.0D, ad4, (j16 - 1) + j2, l16);
+                    Dlaset.dlaset("F", i, j, 0.0, 0.0, ad4, (j16 - 1) + j2, l16);
                     k21 = j16 + l16 * j;
                 } else
                 {
@@ -543,8 +543,8 @@ public final class Dgesdd
                     for(int i25 = ((j - 1) + l3) / l3; i25 > 0; i25--)
                     {
                         int k3 = Math.min((j - l4) + 1, l3);
-                        Dgemm.dgemm("N", "N", i, k3, i, 1.0D, ad4, (j16 - 1) + j2, l16, ad, (1 - 1) + (l4 - 1) * l + k, l, 0.0D, ad4, (k7 - 1) + j2, i);
-                        Dlacpy.dlacpy("F", i, k3, ad4, (k7 - 1) + j2, i, ad, (1 - 1) + (l4 - 1) * l + k, l);
+                        Dgemm.dgemm("N", "N", i, k3, i, 1.0, ad4, (j16 - 1) + j2, l16, ad, (l4 - 1) * l + k, l, 0.0, ad4, (k7 - 1) + j2, i);
+                        Dlacpy.dlacpy("F", i, k3, ad4, (k7 - 1) + j2, i, ad, (l4 - 1) * l + k, l);
                         l4 += l3;
                     }
 
@@ -552,17 +552,17 @@ public final class Dgesdd
             } else
             if(flag5)
             {
-                Dlaset.dlaset("F", i, j, 0.0D, 0.0D, ad3, l1, i2);
+                Dlaset.dlaset("F", i, j, 0.0, 0.0, ad3, l1, i2);
                 Dbdsdc.dbdsdc("L", "I", i, ad1, i1, ad4, (j7 - 1) + j2, ad2, j1, k1, ad3, l1, i2, ad5, 0, ai1, 0, ad4, (k21 - 1) + j2, ai, l2, intw);
                 Dormbr.dormbr("Q", "L", "N", i, i, j, ad, k, l, ad4, (l14 - 1) + j2, ad2, j1, k1, ad4, (k21 - 1) + j2, (k2 - k21) + 1, intw1);
                 Dormbr.dormbr("P", "R", "T", i, j, i, ad, k, l, ad4, (j12 - 1) + j2, ad3, l1, i2, ad4, (k21 - 1) + j2, (k2 - k21) + 1, intw1);
             } else
             if(flag1)
             {
-                Dlaset.dlaset("F", j, j, 0.0D, 0.0D, ad3, l1, i2);
+                Dlaset.dlaset("F", j, j, 0.0, 0.0, ad3, l1, i2);
                 Dbdsdc.dbdsdc("L", "I", i, ad1, i1, ad4, (j7 - 1) + j2, ad2, j1, k1, ad3, l1, i2, ad5, 0, ai1, 0, ad4, (k21 - 1) + j2, ai, l2, intw);
                 if(j > i)
-                    Dlaset.dlaset("F", j - i, j - i, 0.0D, 1.0D, ad3, ((i + 1) - 1) + ((i + 1) - 1) * i2 + l1, i2);
+                    Dlaset.dlaset("F", j - i, j - i, 0.0, 1.0, ad3, i + i * i2 + l1, i2);
                 Dormbr.dormbr("Q", "L", "N", i, i, j, ad, k, l, ad4, (l14 - 1) + j2, ad2, j1, k1, ad4, (k21 - 1) + j2, (k2 - k21) + 1, intw1);
                 Dormbr.dormbr("P", "R", "T", j, j, i, ad, k, l, ad4, (j12 - 1) + j2, ad3, l1, i2, ad4, (k21 - 1) + j2, (k2 - k21) + 1, intw1);
             }
@@ -574,6 +574,6 @@ public final class Dgesdd
             if(d < d3)
                 Dlascl.dlascl("G", 0, 0, d3, d, k18, 1, ad1, i1, k18, intw1);
         }
-        ad4[(1 - 1) + j2] = j18;
+        ad4[j2] = j18;
     }
 }
