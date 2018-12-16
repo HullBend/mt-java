5383ffc309c5ed163d29bfee25584bb3438d2f8d
HullBend <HullBend@users.noreply.github.com>
2018-12-16 19:33:38 +0100

    beautify

--- src/main/java/org/netlib/blas/Dnrm2.java
+++ src/main/java/org/netlib/blas/Dnrm2.java
@@ -1,33 +1,36 @@
 package org.netlib.blas;
 
+// DNRM2 returns the euclidean norm of a vector via the function
+// name, so that DNRM2 := sqrt( x'*x )
 public final class Dnrm2 {
-	public static double dnrm2(int i, double ad[], int j, int k) {
-		double d2 = 0.0D;
-		double d7 = 0.0D;
-		if ((i < 1) || (k < 1))
-			d2 = 0.0D;
-		else if (i == 1) {
-			d2 = Math.abs(ad[(1 - 1) + j]);
-		} else {
-			double d4 = 0.0D;
-			double d6 = 1.0D;
-			int l = 1;
-			for (int i1 = (((1 + (i - 1) * k) - 1) + k) / k; i1 > 0; i1--) {
-				if (ad[(l - 1) + j] != 0.0D) {
-					double d1 = Math.abs(ad[(l - 1) + j]);
-					if (d4 < d1) {
-						d6 = 1.0D + d6 * Math.pow(d4 / d1, 2);
-						d4 = d1;
-					} else {
-						d6 += Math.pow(d1 / d4, 2);
-					}
-				}
-				l += k;
-			}
 
-			d2 = d4 * Math.sqrt(d6);
-		}
-		d7 = d2;
-		return d7;
-	}
+    public static double dnrm2(int n, double[] x, int _x_offset, int incx) {
+
+        double norm = 0.0;
+
+        if (n < 1 || incx < 1) {
+            norm = 0.0;
+        } else if (n == 1) {
+            norm = Math.abs(x[_x_offset]);
+        } else {
+            double scale = 0.0;
+            double ssq = 1.0;
+            int ix = 1;
+            for (int i = (((1 + (n - 1) * incx) - 1) + incx) / incx; i > 0; i--) {
+                if (x[(ix - 1) + _x_offset] != 0.0) {
+                    double absxi = Math.abs(x[(ix - 1) + _x_offset]);
+                    if (scale < absxi) {
+                        ssq = 1.0 + ssq * Math.pow(scale / absxi, 2);
+                        scale = absxi;
+                    } else {
+                        ssq += Math.pow(absxi / scale, 2);
+                    }
+                }
+                ix += incx;
+            }
+
+            norm = scale * Math.sqrt(ssq);
+        }
+        return norm;
+    }
 }
