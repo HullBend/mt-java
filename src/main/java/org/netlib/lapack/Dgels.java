996f441cf595bd9206b5424c03137d555cb3ca72
HullBend <HullBend@users.noreply.github.com>
2018-12-17 03:16:16 +0100

    some cosmetics

--- src/main/java/org/netlib/lapack/Dgels.java
+++ src/main/java/org/netlib/lapack/Dgels.java
@@ -9,6 +9,7 @@ public final class Dgels
     public static void dgels(String s, int i, int j, int k, double ad[], int l, int i1, double ad1[], 
             int j1, int k1, double ad2[], int l1, int i2, intW intw)
     {
+
         int j4;
 label0:
         {
@@ -20,15 +21,13 @@ label0:
             int k3 = 0;
             int i4 = 0;
             j4 = 0;
-            double d = 0.0D;
-            doubleW doublew = new doubleW(0.0D);
-            double d1 = 0.0D;
-            doubleW doublew1 = new doubleW(0.0D);
-            double ad3[] = new double[1];
+            double d = 0.0;
+            double d1 = 0.0;
+            double[] ad3 = new double[1];
             intw.val = 0;
             k3 = Math.min(i, j);
             flag = i2 == -1;
-            if((Lsame.lsame(s, "N") || Lsame.lsame(s, "T")) ^ true)
+            if(!(Lsame.lsame(s, "N") || Lsame.lsame(s, "T")))
                 intw.val = -1;
             else
             if(i < 0)
@@ -46,7 +45,7 @@ label0:
             if(k1 < Util.max(1, i, j))
                 intw.val = -8;
             else
-            if((i2 < Math.max(1, k3 + Math.max(k3, k))) && flag ^ true)
+            if((i2 < Math.max(1, k3 + Math.max(k3, k))) && !flag)
                 intw.val = -10;
             if((intw.val == 0) || (intw.val == -10))
             {
@@ -70,7 +69,7 @@ label0:
                         l3 = Math.max(l3, Ilaenv.ilaenv(1, "DORMLQ", "LN", j, k, i, -1));
                 }
                 j4 = Math.max(1, k3 + Math.max(k3, k) * l3);
-                ad2[(1 - 1) + l1] = j4;
+                ad2[l1] = j4;
             }
             if(intw.val != 0)
             {
@@ -81,15 +80,18 @@ label0:
                 return;
             if(Util.min(i, j, k) == 0)
             {
-                Dlaset.dlaset("Full", Math.max(i, j), k, 0.0D, 0.0D, ad1, j1, k1);
+                Dlaset.dlaset("Full", Math.max(i, j), k, 0.0, 0.0, ad1, j1, k1);
                 return;
             }
+
+            doubleW doublew = new doubleW(0.0);
+            doubleW doublew1 = new doubleW(0.0);
             doublew1.val = Dlamch.dlamch("S") / Dlamch.dlamch("P");
-            doublew.val = 1.0D / doublew1.val;
+            doublew.val = 1.0 / doublew1.val;
             Dlabad.dlabad(doublew1, doublew);
             d = Dlange.dlange("M", i, j, ad, l, i1, ad3, 0);
             byte0 = 0;
-            if((d > 0.0D) && (d < doublew1.val))
+            if((d > 0.0) && (d < doublew1.val))
             {
                 Dlascl.dlascl("G", 0, 0, d, doublew1.val, i, j, ad, l, i1, intw);
                 byte0 = 1;
@@ -99,9 +101,9 @@ label0:
                 Dlascl.dlascl("G", 0, 0, d, doublew.val, i, j, ad, l, i1, intw);
                 byte0 = 2;
             } else
-            if(d == 0.0D)
+            if(d == 0.0)
             {
-                Dlaset.dlaset("F", Math.max(i, j), k, 0.0D, 0.0D, ad1, j1, k1);
+                Dlaset.dlaset("F", Math.max(i, j), k, 0.0, 0.0, ad1, j1, k1);
                 break label0;
             }
             j2 = i;
@@ -109,7 +111,7 @@ label0:
                 j2 = j;
             d1 = Dlange.dlange("M", j2, k, ad1, j1, k1, ad3, 0);
             byte1 = 0;
-            if((d1 > 0.0D) && (d1 < doublew1.val))
+            if((d1 > 0.0) && (d1 < doublew1.val))
             {
                 Dlascl.dlascl("G", 0, 0, d1, doublew1.val, j2, k, ad1, j1, k1, intw);
                 byte1 = 1;
@@ -121,10 +123,10 @@ label0:
             }
             if(i >= j)
             {
-                Dgeqrf.dgeqrf(i, j, ad, l, i1, ad2, (1 - 1) + l1, ad2, ((k3 + 1) - 1) + l1, i2 - k3, intw);
+                Dgeqrf.dgeqrf(i, j, ad, l, i1, ad2, l1, ad2, k3 + l1, i2 - k3, intw);
                 if(flag1 ^ true)
                 {
-                    Dormqr.dormqr("Left", "Transpose", i, k, j, ad, l, i1, ad2, (1 - 1) + l1, ad1, j1, k1, ad2, ((k3 + 1) - 1) + l1, i2 - k3, intw);
+                    Dormqr.dormqr("Left", "Transpose", i, k, j, ad, l, i1, ad2, l1, ad1, j1, k1, ad2, k3 + l1, i2 - k3, intw);
                     Dtrtrs.dtrtrs("Upper", "No transpose", "Non-unit", j, k, ad, l, i1, ad1, j1, k1, intw);
                     if(intw.val > 0)
                         return;
@@ -135,47 +137,47 @@ label0:
                     if(intw.val > 0)
                         return;
                     int i3 = 1;
-                    for(int k4 = (k - 1) + 1; k4 > 0; k4--)
+                    for(int k4 = k; k4 > 0; k4--)
                     {
                         int k2 = j + 1;
                         for(int i5 = (i - (j + 1)) + 1; i5 > 0; i5--)
                         {
-                            ad1[(k2 - 1) + (i3 - 1) * k1 + j1] = 0.0D;
+                            ad1[(k2 - 1) + (i3 - 1) * k1 + j1] = 0.0;
                             k2++;
                         }
 
                         i3++;
                     }
 
-                    Dormqr.dormqr("Left", "No transpose", i, k, j, ad, l, i1, ad2, (1 - 1) + l1, ad1, j1, k1, ad2, ((k3 + 1) - 1) + l1, i2 - k3, intw);
+                    Dormqr.dormqr("Left", "No transpose", i, k, j, ad, l, i1, ad2, l1, ad1, j1, k1, ad2, k3 + l1, i2 - k3, intw);
                     i4 = i;
                 }
             } else
             {
-                Dgelqf.dgelqf(i, j, ad, l, i1, ad2, (1 - 1) + l1, ad2, ((k3 + 1) - 1) + l1, i2 - k3, intw);
-                if(flag1 ^ true)
+                Dgelqf.dgelqf(i, j, ad, l, i1, ad2, l1, ad2, k3 + l1, i2 - k3, intw);
+                if(!flag1)
                 {
                     Dtrtrs.dtrtrs("Lower", "No transpose", "Non-unit", i, k, ad, l, i1, ad1, j1, k1, intw);
                     if(intw.val > 0)
                         return;
                     int j3 = 1;
-                    for(int l4 = (k - 1) + 1; l4 > 0; l4--)
+                    for(int l4 = k; l4 > 0; l4--)
                     {
                         int l2 = i + 1;
                         for(int j5 = (j - (i + 1)) + 1; j5 > 0; j5--)
                         {
-                            ad1[(l2 - 1) + (j3 - 1) * k1 + j1] = 0.0D;
+                            ad1[(l2 - 1) + (j3 - 1) * k1 + j1] = 0.0;
                             l2++;
                         }
 
                         j3++;
                     }
 
-                    Dormlq.dormlq("Left", "Transpose", j, k, i, ad, l, i1, ad2, (1 - 1) + l1, ad1, j1, k1, ad2, ((k3 + 1) - 1) + l1, i2 - k3, intw);
+                    Dormlq.dormlq("Left", "Transpose", j, k, i, ad, l, i1, ad2, l1, ad1, j1, k1, ad2, k3 + l1, i2 - k3, intw);
                     i4 = j;
                 } else
                 {
-                    Dormlq.dormlq("Left", "No transpose", j, k, i, ad, l, i1, ad2, (1 - 1) + l1, ad1, j1, k1, ad2, ((k3 + 1) - 1) + l1, i2 - k3, intw);
+                    Dormlq.dormlq("Left", "No transpose", j, k, i, ad, l, i1, ad2, l1, ad1, j1, k1, ad2, k3 + l1, i2 - k3, intw);
                     Dtrtrs.dtrtrs("Lower", "Transpose", "Non-unit", i, k, ad, l, i1, ad1, j1, k1, intw);
                     if(intw.val > 0)
                         return;
@@ -193,6 +195,6 @@ label0:
             if(byte1 == 2)
                 Dlascl.dlascl("G", 0, 0, doublew.val, d1, i4, k, ad1, j1, k1, intw);
         }
-        ad2[(1 - 1) + l1] = j4;
+        ad2[l1] = j4;
     }
 }
--- src/main/java/org/netlib/lapack/Dlange.java
+++ src/main/java/org/netlib/lapack/Dlange.java
@@ -4,82 +4,83 @@ import org.netlib.blas.Lsame;
 import org.netlib.util.doubleW;
 
 public final class Dlange {
-	public static double dlange(String s, int i, int j, double ad[], int k,
-			int l, double ad1[], int i1) {
-		doubleW doublew = new doubleW(0.0D);
-		doubleW doublew1 = new doubleW(0.0D);
-		double d = 0.0D;
-		double d1 = 0.0D;
-		if (Math.min(i, j) == 0)
-			d = 0.0D;
-		else if (Lsame.lsame(s, "M")) {
-			d = 0.0D;
-			int i2 = 1;
-			for (int i3 = (j - 1) + 1; i3 > 0; i3--) {
-				int j1 = 1;
-				for (int k4 = (i - 1) + 1; k4 > 0; k4--) {
-					d = Math.max(d, Math.abs(ad[(j1 - 1) + (i2 - 1) * l + k]));
-					j1++;
-				}
 
-				i2++;
-			}
+    public static double dlange(String s, int i, int j, double ad[], int k,
+            int l, double ad1[], int i1) {
 
-		} else if (Lsame.lsame(s, "O") || s.regionMatches(0, "1", 0, 1)) {
-			d = 0.0D;
-			int j2 = 1;
-			for (int j3 = (j - 1) + 1; j3 > 0; j3--) {
-				doublew1.val = 0.0D;
-				int k1 = 1;
-				for (int l4 = (i - 1) + 1; l4 > 0; l4--) {
-					doublew1.val = doublew1.val
-							+ Math.abs(ad[(k1 - 1) + (j2 - 1) * l + k]);
-					k1++;
-				}
+        double d = 0.0;
 
-				d = Math.max(d, doublew1.val);
-				j2++;
-			}
+        if (Math.min(i, j) == 0)
+            d = 0.0;
+        else if (Lsame.lsame(s, "M")) {
+            d = 0.0;
+            int i2 = 1;
+            for (int i3 = j; i3 > 0; i3--) {
+                int j1 = 1;
+                for (int k4 = i; k4 > 0; k4--) {
+                    d = Math.max(d, Math.abs(ad[j1 - 1 + (i2 - 1) * l + k]));
+                    j1++;
+                }
 
-		} else if (Lsame.lsame(s, "I")) {
-			int l1 = 1;
-			for (int k3 = (i - 1) + 1; k3 > 0; k3--) {
-				ad1[(l1 - 1) + i1] = 0.0D;
-				l1++;
-			}
+                i2++;
+            }
 
-			int k2 = 1;
-			for (int l3 = (j - 1) + 1; l3 > 0; l3--) {
-				l1 = 1;
-				for (int i5 = (i - 1) + 1; i5 > 0; i5--) {
-					ad1[(l1 - 1) + i1] = ad1[(l1 - 1) + i1]
-							+ Math.abs(ad[(l1 - 1) + (k2 - 1) * l + k]);
-					l1++;
-				}
+        } else if (Lsame.lsame(s, "O") || s.regionMatches(0, "1", 0, 1)) {
+            d = 0.0;
+            int j2 = 1;
+            doubleW dw = new doubleW(0.0);
+            for (int j3 = j; j3 > 0; j3--) {
+                dw.val = 0.0;
+                int k1 = 1;
+                for (int l4 = i; l4 > 0; l4--) {
+                    dw.val = dw.val
+                            + Math.abs(ad[k1 - 1 + (j2 - 1) * l + k]);
+                    k1++;
+                }
 
-				k2++;
-			}
+                d = Math.max(d, dw.val);
+                j2++;
+            }
 
-			d = 0.0D;
-			l1 = 1;
-			for (int i4 = (i - 1) + 1; i4 > 0; i4--) {
-				d = Math.max(d, ad1[(l1 - 1) + i1]);
-				l1++;
-			}
+        } else if (Lsame.lsame(s, "I")) {
+            int l1 = 1;
+            for (int k3 = i; k3 > 0; k3--) {
+                ad1[l1 - 1 + i1] = 0.0;
+                l1++;
+            }
 
-		} else if (Lsame.lsame(s, "F") || Lsame.lsame(s, "E")) {
-			doublew.val = 0.0D;
-			doublew1.val = 1.0D;
-			int l2 = 1;
-			for (int j4 = (j - 1) + 1; j4 > 0; j4--) {
-				Dlassq.dlassq(i, ad, (1 - 1) + (l2 - 1) * l + k, 1, doublew,
-						doublew1);
-				l2++;
-			}
+            int k2 = 1;
+            for (int l3 = j; l3 > 0; l3--) {
+                l1 = 1;
+                for (int i5 = i; i5 > 0; i5--) {
+                    ad1[l1 - 1 + i1] = ad1[l1 - 1 + i1]
+                            + Math.abs(ad[l1 - 1 + (k2 - 1) * l + k]);
+                    l1++;
+                }
 
-			d = doublew.val * Math.sqrt(doublew1.val);
-		}
-		d1 = d;
-		return d1;
-	}
+                k2++;
+            }
+
+            d = 0.0;
+            l1 = 1;
+            for (int i4 = i; i4 > 0; i4--) {
+                d = Math.max(d, ad1[l1 - 1 + i1]);
+                l1++;
+            }
+
+        } else if (Lsame.lsame(s, "F") || Lsame.lsame(s, "E")) {
+            doubleW dw1 = new doubleW(1.0);
+            doubleW dw2 = new doubleW(0.0);
+            
+            int l2 = 1;
+            for (int j4 = j; j4 > 0; j4--) {
+                Dlassq.dlassq(i, ad, (l2 - 1) * l + k, 1, dw2,
+                        dw1);
+                l2++;
+            }
+
+            d = dw2.val * Math.sqrt(dw1.val);
+        }
+        return d;
+    }
 }
