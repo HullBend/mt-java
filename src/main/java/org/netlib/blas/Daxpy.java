ee254ed8eb2a4b0fc3fe3cda9f7cca1db5acd8db
HullBend <HullBend@users.noreply.github.com>
2018-12-18 02:55:27 +0100

    optimize

--- src/main/java/org/netlib/blas/Daxpy.java
+++ src/main/java/org/netlib/blas/Daxpy.java
@@ -1,7 +1,6 @@
 package org.netlib.blas;
 
 // constant times a vector plus a vector.
-// uses unrolled loops for increments equal to one.
 public final class Daxpy {
 
     public static void daxpy(int n, double da, double[] dx, int _dx_offset, int incx,
@@ -10,8 +9,6 @@ public final class Daxpy {
         if (n <= 0 || da == 0.0) {
             return;
         }
-
-        int i = 0;
 
         if (incx != 1 || incy != 1) {
             // code for unequal increments or equal increments
@@ -24,37 +21,18 @@ public final class Daxpy {
             if (incy < 0) {
                 iy = (-n + 1) * incy + 1;
             }
-            i = 1;
             for (int k = n; k > 0; k--) {
                 dy[(iy - 1) + _dy_offset] = dy[(iy - 1) + _dy_offset] + da * dx[(ix - 1) + _dx_offset];
                 ix += incx;
                 iy += incy;
-                i++;
             }
 
             return;
         }
-        // code for both increments equal to 1
-        int m = n % 4; // label 20
-        if (m != 0) {
-            i = 1;
-            for (int k = m; k > 0; k--) {
-                dy[(i - 1) + _dy_offset] = dy[(i - 1) + _dy_offset] + da * dx[(i - 1) + _dx_offset];
-                i++;
-            }
 
-            if (n < 4) {
-                return;
-            }
-        }
-        int mp1 = m + 1; // label 40
-        i = mp1;
-        for (int k = ((n - mp1) + 4) / 4; k > 0; k--) {
-            dy[i - 1 + _dy_offset] = dy[i - 1 + _dy_offset] + da * dx[i - 1 + _dx_offset];
-            dy[i +     _dy_offset] = dy[i     + _dy_offset] + da * dx[i     + _dx_offset];
-            dy[i + 1 + _dy_offset] = dy[i + 1 + _dy_offset] + da * dx[i + 1 + _dx_offset];
-            dy[i + 2 + _dy_offset] = dy[i + 2 + _dy_offset] + da * dx[i + 2 + _dx_offset];
-            i += 4;
+        // code for both increments equal to 1
+        for (int j = 0; j < n; ++j) {
+            dy[j + _dy_offset] += da * dx[j + _dx_offset];
         }
     }
 }
