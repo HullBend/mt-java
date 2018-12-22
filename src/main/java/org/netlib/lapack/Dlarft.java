6084ad75726be1d19edf5109c833290f08f18d69
HullBend <HullBend@users.noreply.github.com>
2018-12-22 20:22:01 +0100

    beautify

--- src/main/java/org/netlib/lapack/Dlarft.java
+++ src/main/java/org/netlib/lapack/Dlarft.java
@@ -4,87 +4,94 @@ import org.netlib.blas.Dgemv;
 import org.netlib.blas.Dtrmv;
 import org.netlib.blas.Lsame;
 
+//DLARFT forms the triangular factor T of a real block reflector H
+//of order n, which is defined as a product of k elementary reflectors.
+//
+//If DIRECT = 'F', H = H(1) H(2) . . . H(k) and T is upper triangular;
+//
+//If DIRECT = 'B', H = H(k) . . . H(2) H(1) and T is lower triangular.
+//
+//If STOREV = 'C', the vector which defines the elementary reflector
+//H(i) is stored in the i-th column of the array V, and
+//
+//   H  =  I - V * T * V**T
+//
+//If STOREV = 'R', the vector which defines the elementary reflector
+//H(i) is stored in the i-th row of the array V, and
+//
+//   H  =  I - V**T * T * V
 public final class Dlarft {
-	public static void dlarft(String s, String s1, int i, int j, double ad[],
-			int k, int l, double ad1[], int i1, double ad2[], int j1, int k1) {
-		if (i == 0)
-			return;
-		if (Lsame.lsame(s, "F")) {
-			int l1 = 1;
-			for (int l2 = (j - 1) + 1; l2 > 0; l2--) {
-				if (ad1[(l1 - 1) + i1] == 0.0D) {
-					int j2 = 1;
-					for (int j3 = (l1 - 1) + 1; j3 > 0; j3--) {
-						ad2[(j2 - 1) + (l1 - 1) * k1 + j1] = 0.0D;
-						j2++;
-					}
 
-				} else {
-					double d1 = ad[(l1 - 1) + (l1 - 1) * l + k];
-					ad[(l1 - 1) + (l1 - 1) * l + k] = 1.0D;
-					if (Lsame.lsame(s1, "C"))
-						Dgemv.dgemv("Transpose", (i - l1) + 1, l1 - 1,
-								-ad1[(l1 - 1) + i1], ad, (l1 - 1) + (1 - 1) * l
-										+ k, l, ad,
-								(l1 - 1) + (l1 - 1) * l + k, 1, 0.0D, ad2,
-								(1 - 1) + (l1 - 1) * k1 + j1, 1);
-					else
-						Dgemv.dgemv("No transpose", l1 - 1, (i - l1) + 1,
-								-ad1[(l1 - 1) + i1], ad, (1 - 1) + (l1 - 1) * l
-										+ k, l, ad,
-								(l1 - 1) + (l1 - 1) * l + k, l, 0.0D, ad2,
-								(1 - 1) + (l1 - 1) * k1 + j1, 1);
-					ad[(l1 - 1) + (l1 - 1) * l + k] = d1;
-					Dtrmv.dtrmv("Upper", "No transpose", "Non-unit", l1 - 1,
-							ad2, j1, k1, ad2, (1 - 1) + (l1 - 1) * k1 + j1, 1);
-					ad2[(l1 - 1) + (l1 - 1) * k1 + j1] = ad1[(l1 - 1) + i1];
-				}
-				l1++;
-			}
+    public static void dlarft(String direct, String storev, int n, int k, double[] v, int _v_offset, int ldv,
+            double[] tau, int _tau_offset, double[] t, int _t_offset, int ldt) {
 
-		} else {
-			int i2 = j;
-			for (int i3 = ((1 - j) + -1) / -1; i3 > 0; i3--) {
-				if (ad1[(i2 - 1) + i1] == 0.0D) {
-					int k2 = i2;
-					for (int k3 = (j - i2) + 1; k3 > 0; k3--) {
-						ad2[(k2 - 1) + (i2 - 1) * k1 + j1] = 0.0D;
-						k2++;
-					}
+        if (n == 0) {
+            return;
+        }
+        if (Lsame.lsame(direct, "F")) {
+            int I = 1;
+            for (int p = k; p > 0; p--) {
+                if (tau[(I - 1) + _tau_offset] == 0.0) {
+                    int J = 1;
+                    for (int q = I; q > 0; q--) {
+                        t[(J - 1) + (I - 1) * ldt + _t_offset] = 0.0;
+                        J++;
+                    }
 
-				} else {
-					if (i2 < j) {
-						if (Lsame.lsame(s1, "C")) {
-							double d2 = ad[(((i - j) + i2) - 1) + (i2 - 1) * l
-									+ k];
-							ad[(((i - j) + i2) - 1) + (i2 - 1) * l + k] = 1.0D;
-							Dgemv.dgemv("Transpose", (i - j) + i2, j - i2,
-									-ad1[(i2 - 1) + i1], ad, (1 - 1)
-											+ ((i2 + 1) - 1) * l + k, l, ad,
-									(1 - 1) + (i2 - 1) * l + k, 1, 0.0D, ad2,
-									((i2 + 1) - 1) + (i2 - 1) * k1 + j1, 1);
-							ad[(((i - j) + i2) - 1) + (i2 - 1) * l + k] = d2;
-						} else {
-							double d3 = ad[(i2 - 1) + (((i - j) + i2) - 1) * l
-									+ k];
-							ad[(i2 - 1) + (((i - j) + i2) - 1) * l + k] = 1.0D;
-							Dgemv.dgemv("No transpose", j - i2, (i - j) + i2,
-									-ad1[(i2 - 1) + i1], ad, ((i2 + 1) - 1)
-											+ (1 - 1) * l + k, l, ad, (i2 - 1)
-											+ (1 - 1) * l + k, l, 0.0D, ad2,
-									((i2 + 1) - 1) + (i2 - 1) * k1 + j1, 1);
-							ad[(i2 - 1) + (((i - j) + i2) - 1) * l + k] = d3;
-						}
-						Dtrmv.dtrmv("Lower", "No transpose", "Non-unit",
-								j - i2, ad2, ((i2 + 1) - 1) + ((i2 + 1) - 1)
-										* k1 + j1, k1, ad2, ((i2 + 1) - 1)
-										+ (i2 - 1) * k1 + j1, 1);
-					}
-					ad2[(i2 - 1) + (i2 - 1) * k1 + j1] = ad1[(i2 - 1) + i1];
-				}
-				i2--;
-			}
+                } else {
+                    double VII = v[(I - 1) + (I - 1) * ldv + _v_offset];
+                    v[(I - 1) + (I - 1) * ldv + _v_offset] = 1.0;
+                    if (Lsame.lsame(storev, "C")) {
+                        Dgemv.dgemv("Transpose", (n - I) + 1, I - 1, -tau[(I - 1) + _tau_offset], v,
+                                (I - 1) + _v_offset, ldv, v, (I - 1) + (I - 1) * ldv + _v_offset, 1, 0.0, t,
+                                (I - 1) * ldt + _t_offset, 1);
+                    } else {
+                        Dgemv.dgemv("No transpose", I - 1, (n - I) + 1, -tau[(I - 1) + _tau_offset], v,
+                                (I - 1) * ldv + _v_offset, ldv, v, (I - 1) + (I - 1) * ldv + _v_offset, ldv, 0.0, t,
+                                (I - 1) * ldt + _t_offset, 1);
+                    }
+                    v[(I - 1) + (I - 1) * ldv + _v_offset] = VII;
+                    Dtrmv.dtrmv("Upper", "No transpose", "Non-unit", I - 1, t, _t_offset, ldt, t,
+                            (I - 1) * ldt + _t_offset, 1);
+                    t[(I - 1) + (I - 1) * ldt + _t_offset] = tau[(I - 1) + _tau_offset];
+                }
+                I++;
+            }
 
-		}
-	}
+        } else {
+            int I = k;
+            for (int p = k; p > 0; p--) {
+                if (tau[(I - 1) + _tau_offset] == 0.0) {
+                    int J = I;
+                    for (int q = (k - I) + 1; q > 0; q--) {
+                        t[(J - 1) + (I - 1) * ldt + _t_offset] = 0.0;
+                        J++;
+                    }
+
+                } else {
+                    if (I < k) {
+                        if (Lsame.lsame(storev, "C")) {
+                            double VII = v[(((n - k) + I) - 1) + (I - 1) * ldv + _v_offset];
+                            v[(((n - k) + I) - 1) + (I - 1) * ldv + _v_offset] = 1.0;
+                            Dgemv.dgemv("Transpose", (n - k) + I, k - I, -tau[(I - 1) + _tau_offset], v,
+                                    I * ldv + _v_offset, ldv, v, (I - 1) * ldv + _v_offset, 1, 0.0, t,
+                                    I + (I - 1) * ldt + _t_offset, 1);
+                            v[(((n - k) + I) - 1) + (I - 1) * ldv + _v_offset] = VII;
+                        } else {
+                            double VII = v[(I - 1) + (((n - k) + I) - 1) * ldv + _v_offset];
+                            v[(I - 1) + (((n - k) + I) - 1) * ldv + _v_offset] = 1.0;
+                            Dgemv.dgemv("No transpose", k - I, (n - k) + I, -tau[(I - 1) + _tau_offset], v,
+                                    I + _v_offset, ldv, v, (I - 1) + _v_offset, ldv, 0.0, t,
+                                    I + (I - 1) * ldt + _t_offset, 1);
+                            v[(I - 1) + (((n - k) + I) - 1) * ldv + _v_offset] = VII;
+                        }
+                        Dtrmv.dtrmv("Lower", "No transpose", "Non-unit", k - I, t, I + I * ldt + _t_offset, ldt, t,
+                                I + (I - 1) * ldt + _t_offset, 1);
+                    }
+                    t[(I - 1) + (I - 1) * ldt + _t_offset] = tau[(I - 1) + _tau_offset];
+                }
+                I--;
+            }
+        }
+    }
 }
