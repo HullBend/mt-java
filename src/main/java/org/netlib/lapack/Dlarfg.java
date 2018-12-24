2040cf6363dc0648e3723a55a8e29a163fb02c66
HullBend <HullBend@users.noreply.github.com>
2018-12-24 03:11:00 +0100

    beautify

--- src/main/java/org/netlib/lapack/Dlarfg.java
+++ src/main/java/org/netlib/lapack/Dlarfg.java
@@ -5,44 +5,67 @@ import org.netlib.blas.Dscal;
 import org.netlib.util.Util;
 import org.netlib.util.doubleW;
 
+// DLARFG generates a real elementary reflector H of order n, such
+// that
+//
+//       H * ( alpha ) = ( beta ),   H**T * H = I.
+//           (   x   )   (   0  )
+//
+// where alpha and beta are scalars, and x is an (n-1)-element real
+// vector. H is represented in the form
+//
+//       H = I - tau * ( 1 ) * ( 1 v**T ) ,
+//                     ( v )
+//
+// where tau is a real scalar and v is a real (n-1)-element
+// vector.
+//
+// If the elements of x are all zero, then tau = 0 and H is taken
+// to be the unit matrix.
+//
+// Otherwise  1 <= tau <= 2.
 public final class Dlarfg {
 
-	public static void dlarfg(int i, doubleW doublew, double[] ad, int j,
-			int k, doubleW doublew1) {
+    public static void dlarfg(int n, doubleW alpha, double[] x, int _x_offset, int incx, doubleW tau) {
 
-		double d6 = 0.0;
-		if (i <= 1) {
-			doublew1.val = 0.0;
-			return;
-		}
-		d6 = Dnrm2.dnrm2(i - 1, ad, j, k);
-		if (d6 == 0.0) {
-			doublew1.val = 0.0;
-		} else {
-			double d1 = -Util.dsign(Dlapy2.dlapy2(doublew.val, d6), doublew.val);
-			final double d5 = Dlamch.dlamch("S") / Dlamch.dlamch("E"); // XXX
-			if (Math.abs(d1) < d5) {
-				double d3 = 1.0 / d5;
-				int i1 = 0;
-				do {
-					i1++;
-					Dscal.dscal(i - 1, d3, ad, j, k);
-					d1 *= d3;
-					doublew.val = doublew.val * d3;
-				} while (Math.abs(d1) < d5);
-				double d7 = Dnrm2.dnrm2(i - 1, ad, j, k);
-				d1 = -Util.dsign(Dlapy2.dlapy2(doublew.val, d7), doublew.val);
-				doublew1.val = (d1 - doublew.val) / d1;
-				Dscal.dscal(i - 1, 1.0 / (doublew.val - d1), ad, j, k);
-				doublew.val = d1;
-				for (int j1 = i1; j1 > 0; j1--) {
-					doublew.val = doublew.val * d5;
-				}
-			} else {
-				doublew1.val = (d1 - doublew.val) / d1;
-				Dscal.dscal(i - 1, 1.0 / (doublew.val - d1), ad, j, k);
-				doublew.val = d1;
-			}
-		}
-	}
+        if (n <= 1) {
+            tau.val = 0.0;
+            return;
+        }
+        double xnorm = Dnrm2.dnrm2(n - 1, x, _x_offset, incx);
+        if (xnorm == 0.0) {
+            // H = I
+            tau.val = 0.0;
+        } else {
+            // general case
+            double beta = -Util.dsign(Dlapy2.dlapy2(alpha.val, xnorm), alpha.val);
+            final double safmin = 2.0041683600089728E-292;
+            if (Math.abs(beta) < safmin) {
+                final double rsafmn = 4.9896007738367995E291;
+                int knt = 0;
+                do {
+                    knt++;
+                    Dscal.dscal(n - 1, rsafmn, x, _x_offset, incx);
+                    beta *= rsafmn;
+                    alpha.val = alpha.val * rsafmn;
+                } while (Math.abs(beta) < safmin);
+
+                // New BETA is at most 1, at least SAFMIN
+
+                xnorm = Dnrm2.dnrm2(n - 1, x, _x_offset, incx);
+                beta = -Util.dsign(Dlapy2.dlapy2(alpha.val, xnorm), alpha.val);
+                tau.val = (beta - alpha.val) / beta;
+                Dscal.dscal(n - 1, 1.0 / (alpha.val - beta), x, _x_offset, incx);
+                alpha.val = beta;
+                // If ALPHA is subnormal, it may lose relative accuracy
+                for (int j = knt; j > 0; j--) {
+                    alpha.val = alpha.val * safmin;
+                }
+            } else {
+                tau.val = (beta - alpha.val) / beta;
+                Dscal.dscal(n - 1, 1.0 / (alpha.val - beta), x, _x_offset, incx);
+                alpha.val = beta;
+            }
+        }
+    }
 }
