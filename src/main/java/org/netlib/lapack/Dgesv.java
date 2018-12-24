40ce25d1c02ba1bf397822b3a8b776ff90097b0f
HullBend <HullBend@users.noreply.github.com>
2018-12-24 03:20:57 +0100

    beautify

--- src/main/java/org/netlib/lapack/Dgesv.java
+++ src/main/java/org/netlib/lapack/Dgesv.java
@@ -3,32 +3,42 @@ package org.netlib.lapack;
 import org.netlib.err.Xerbla;
 import org.netlib.util.intW;
 
-public final class Dgesv
-{
-    public static void dgesv(int i, int j, double[] ad, int k, int l, int[] ai, int i1, double[] ad1, 
-            int j1, int k1, intW intw)
-    {
-        intw.val = 0;
-        if (i < 0)
-            intw.val = -1;
-        else
-        if (j < 0)
-            intw.val = -2;
-        else
-        if (l < Math.max(1, i))
-            intw.val = -4;
-        else
-        if (k1 < Math.max(1, i))
-            intw.val = -7;
+// DGESV computes the solution to a real system of linear equations
+//    A * X = B,
+// where A is an N-by-N matrix and X and B are N-by-NRHS matrices.
+//
+// The LU decomposition with partial pivoting and row interchanges is
+// used to factor A as
+//    A = P * L * U,
+// where P is a permutation matrix, L is unit lower triangular, and U is
+// upper triangular.  The factored form of A is then used to solve the
+// system of equations A * X = B.
+public final class Dgesv {
 
-        if (intw.val != 0)
-        {
-            Xerbla.xerbla("DGESV ", -intw.val);
+    public static void dgesv(int n, int nrhs, double[] a, int _a_offset, int lda, int[] ipiv, int _ipiv_offset,
+            double[] b, int _b_offset, int ldb, intW info) {
+
+        info.val = 0;
+        if (n < 0) {
+            info.val = -1;
+        } else if (nrhs < 0) {
+            info.val = -2;
+        } else if (lda < Math.max(1, n)) {
+            info.val = -4;
+        } else if (ldb < Math.max(1, n)) {
+            info.val = -7;
+        }
+
+        if (info.val != 0) {
+            Xerbla.xerbla("DGESV ", -info.val);
             return;
         }
-        Dgetrf.dgetrf(i, i, ad, k, l, ai, i1, intw);
-        if (intw.val == 0) {
-            Dgetrs.dgetrs("No transpose", i, j, ad, k, l, ai, i1, ad1, j1, k1, intw);
+
+        // Compute the LU factorization of A
+        Dgetrf.dgetrf(n, n, a, _a_offset, lda, ipiv, _ipiv_offset, info);
+        if (info.val == 0) {
+            // Solve the system A*X = B, overwriting B with X
+            Dgetrs.dgetrs("No transpose", n, nrhs, a, _a_offset, lda, ipiv, _ipiv_offset, b, _b_offset, ldb, info);
         }
     }
 }
