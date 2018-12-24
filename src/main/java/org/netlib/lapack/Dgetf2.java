689e0b65ec9a90ec7ee5c72924af0978fa86a2f0
HullBend <HullBend@users.noreply.github.com>
2018-12-24 02:28:40 +0100

    beautify

--- src/main/java/org/netlib/lapack/Dgetf2.java
+++ src/main/java/org/netlib/lapack/Dgetf2.java
@@ -4,62 +4,70 @@ import org.netlib.blas.*;
 import org.netlib.err.Xerbla;
 import org.netlib.util.intW;
 
-public final class Dgetf2
-{
-    public static void dgetf2(int i, int j, double[] ad, int k, int l, int[] ai, int i1, intW intw)
-    {
+// DGETF2 computes an LU factorization of a general m-by-n matrix A
+// using partial pivoting with row interchanges.
+//
+// The factorization has the form
+//    A = P * L * U
+// where P is a permutation matrix, L is lower triangular with unit
+// diagonal elements (lower trapezoidal if m > n), and U is upper
+// triangular (upper trapezoidal if m < n).
+public final class Dgetf2 {
 
-        intw.val = 0;
-		if (i < 0) {
-			intw.val = -1;
-		} else if (j < 0) {
-			intw.val = -2;
-		} else if (l < Math.max(1, i)) {
-			intw.val = -4;
-		}
+    public static void dgetf2(int m, int n, double[] a, int _a_offset, int lda, int[] ipiv, int _ipiv_offset,
+            intW info) {
 
-        if (intw.val != 0)
-        {
-            Xerbla.xerbla("DGETF2", -intw.val);
+        info.val = 0;
+        if (m < 0) {
+            info.val = -1;
+        } else if (n < 0) {
+            info.val = -2;
+        } else if (lda < Math.max(1, m)) {
+            info.val = -4;
+        }
+
+        if (info.val != 0) {
+            Xerbla.xerbla("DGETF2", -info.val);
             return;
         }
-        if (i == 0 || j == 0) {
+        // Quick return if possible
+        if (m == 0 || n == 0) {
             return;
         }
 
-        int k1 = 1;
-        for (int p = Math.min(i, j); p > 0; p--)
-        {
-            int l1 = k1 - 1 + Idamax.idamax(i - k1 + 1, ad, k1 - 1 + (k1 - 1) * l + k, 1);
-            ai[k1 - 1 + i1] = l1;
-            if (ad[l1 - 1 + (k1 - 1) * l + k] != 0.0)
-            {
-                if (l1 != k1) {
-                    Dswap.dswap(j, ad, k1 - 1 + k, l, ad, l1 - 1 + k, l);
+        int j = 1;
+        for (int p = Math.min(m, n); p > 0; p--) {
+            // Find pivot and test for singularity
+            int jp = j - 1 + Idamax.idamax(m - j + 1, a, j - 1 + (j - 1) * lda + _a_offset, 1);
+            ipiv[j - 1 + _ipiv_offset] = jp;
+            if (a[jp - 1 + (j - 1) * lda + _a_offset] != 0.0) {
+                // Apply the interchange to columns 1:N
+                if (jp != j) {
+                    Dswap.dswap(n, a, j - 1 + _a_offset, lda, a, jp - 1 + _a_offset, lda);
                 }
-                if (k1 < i) {
-                    if (Math.abs(ad[k1 - 1 + (k1 - 1) * l + k]) >= 2.2250738585072014E-308)
-                    {
-                        Dscal.dscal(i - k1, 1.0 / ad[k1 - 1 + (k1 - 1) * l + k], ad, k1 + (k1 - 1) * l + k, 1);
-                    } else
-                    {
-                        int j1 = 1;
-                        for (int q = i - k1; q > 0; q--)
-                        {
-                            ad[k1 + j1 - 1 + (k1 - 1) * l + k] = ad[k1 + j1 - 1 + (k1 - 1) * l + k] / ad[k1 - 1 + (k1 - 1) * l + k];
-                            j1++;
+                // Compute elements J+1:M of J-th column
+                if (j < m) {
+                    if (Math.abs(a[j - 1 + (j - 1) * lda + _a_offset]) >= 2.2250738585072014E-308) {
+                        Dscal.dscal(m - j, 1.0 / a[j - 1 + (j - 1) * lda + _a_offset], a, j + (j - 1) * lda + _a_offset,
+                                1);
+                    } else {
+                        int i = 1;
+                        for (int q = m - j; q > 0; q--) {
+                            a[j + i - 1 + (j - 1) * lda + _a_offset] = a[j + i - 1 + (j - 1) * lda + _a_offset]
+                                    / a[j - 1 + (j - 1) * lda + _a_offset];
+                            i++;
                         }
-
                     }
                 }
-            } else
-            if (intw.val == 0) {
-                intw.val = k1;
+            } else if (info.val == 0) {
+                info.val = j;
             }
-            if (k1 < Math.min(i, j)) {
-                Dger.dger(i - k1, j - k1, -1.0, ad, k1 + (k1 - 1) * l + k, 1, ad, k1 - 1 + k1 * l + k, l, ad, k1 + k1 * l + k, l);
+            if (j < Math.min(m, n)) {
+                // Update trailing submatrix
+                Dger.dger(m - j, n - j, -1.0, a, j + (j - 1) * lda + _a_offset, 1, a, j - 1 + j * lda + _a_offset, lda,
+                        a, j + j * lda + _a_offset, lda);
             }
-            k1++;
+            j++;
         }
     }
 }
