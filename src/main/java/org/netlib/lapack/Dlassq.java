a2ef878ab707a2254c6111343577a6bcaf2f29d2
HullBend <HullBend@users.noreply.github.com>
2018-12-24 03:31:38 +0100

    beautify

--- src/main/java/org/netlib/lapack/Dlassq.java
+++ src/main/java/org/netlib/lapack/Dlassq.java
@@ -2,27 +2,37 @@ package org.netlib.lapack;
 
 import org.netlib.util.doubleW;
 
+// DLASSQ  returns the values  scl and smsq such that
+//
+//    ( scl**2 )*smsq = x( 1 )**2 +...+ x( n )**2 + ( scale**2 )*sumsq,
+//
+// where  x( i ) = X( 1 + ( i - 1 )*INCX ). The value of sumsq is
+// assumed to be non-negative and scl returns the value
+//
+//    scl = max( scale, abs( x( i ) ) ).
+//
+// scale and sumsq must be supplied in SCALE and SUMSQ and
+// scl and smsq are overwritten on SCALE and SUMSQ respectively.
+//
+// The routine makes only one pass through the vector x.
 public final class Dlassq {
 
-	public static void dlassq(int i, double[] ad, int j, int k,
-			doubleW doublew, doubleW doublew1) {
+    public static void dlassq(int n, double[] x, int _x_offset, int incx, doubleW scale, doubleW sumsq) {
 
-		if (i > 0) {
-			int l = 1;
-			for (int i1 = (((1 + (i - 1) * k) - 1) + k) / k; i1 > 0; i1--) {
-				if (ad[l - 1 + j] != 0.0) {
-					double d1 = Math.abs(ad[l - 1 + j]);
-					if (doublew.val < d1) {
-						doublew1.val = 1 + doublew1.val
-								* Math.pow(doublew.val / d1, 2);
-						doublew.val = d1;
-					} else {
-						doublew1.val = doublew1.val
-								+ Math.pow(d1 / doublew.val, 2);
-					}
-				}
-				l += k;
-			}
-		}
-	}
+        if (n > 0) {
+            int ix = 1;
+            for (int p = ((n - 1) * incx + incx) / incx; p > 0; p--) {
+                if (x[ix - 1 + _x_offset] != 0.0) {
+                    double absxi = Math.abs(x[ix - 1 + _x_offset]);
+                    if (scale.val < absxi) {
+                        sumsq.val = 1.0 + sumsq.val * Math.pow(scale.val / absxi, 2.0);
+                        scale.val = absxi;
+                    } else {
+                        sumsq.val = sumsq.val + Math.pow(absxi / scale.val, 2.0);
+                    }
+                }
+                ix += incx;
+            }
+        }
+    }
 }
