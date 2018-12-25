6ed36f5972067bf92968482891ea61730c1bd332
HullBend <HullBend@users.noreply.github.com>
2018-12-25 01:12:32 +0100

    simplify

--- src/main/java/org/netlib/blas/Ddot.java
+++ src/main/java/org/netlib/blas/Ddot.java
@@ -1,7 +1,6 @@
 package org.netlib.blas;
 
 // DDOT forms the dot product of two vectors.
-// Uses unrolled loops for increments equal to one.
 public final class Ddot {
 
     public static double ddot(int n, double[] dx, int _dx_offset, int incx, double[] dy,
@@ -11,58 +10,32 @@ public final class Ddot {
             return 0.0;
         }
 
-        double ddot;
-        label0: {
+        double ddot = 0.0;
 
-            int j = 0;
-            ddot = 0.0;
-
-            // code for unequal increments or equal increments
-            // not equal to 1
-            if (incx != 1 || incy != 1) {
-                int k = 1;
-                int l = 1;
-                if (incx < 0) {
-                    k = (-n + 1) * incx + 1;
-                }
-                if (incy < 0) {
-                    l = (-n + 1) * incy + 1;
-                }
-                j = 1;
-                for (int q = n; q > 0; q--) {
-                    ddot += dx[k - 1 + _dx_offset] * dy[l - 1 + _dy_offset];
-                    k += incx;
-                    l += incy;
-                    j++;
-                }
-
-                return ddot;
+        // code for unequal increments or equal increments
+        // not equal to 1
+        if (incx != 1 || incy != 1) {
+            int k = 1;
+            int l = 1;
+            if (incx < 0) {
+                k = (-n + 1) * incx + 1;
             }
-            // code for both increments equal to 1
-            int m = n % 5;
-            if (m != 0) {
-                j = 1;
-                for (int q = m; q > 0; q--) {
-                    ddot += dx[j - 1 + _dx_offset] * dy[j - 1 + _dy_offset];
-                    j++;
-                }
-
-                if (n < 5) {
-                    break label0;
-                }
-            }
-            int mp1 = m + 1;
-            j = mp1;
-            for (int q = (n - mp1 + 5) / 5; q > 0; q--) {
-                ddot = ddot
-                        + dx[j - 1 + _dx_offset] * dy[j - 1 + _dy_offset]
-                        + dx[j     + _dx_offset] * dy[j     + _dy_offset]
-                        + dx[j + 1 + _dx_offset] * dy[j + 1 + _dy_offset]
-                        + dx[j + 2 + _dx_offset] * dy[j + 2 + _dy_offset]
-                        + dx[j + 3 + _dx_offset] * dy[j + 3 + _dy_offset];
-                j += 5;
+            if (incy < 0) {
+                l = (-n + 1) * incy + 1;
             }
 
+            for (int q = n; q > 0; q--) {
+                ddot += dx[k - 1 + _dx_offset] * dy[l - 1 + _dy_offset];
+                k += incx;
+                l += incy;
+            }
+
+            return ddot;
+        }
+
+        // code for both increments equal to 1
+        for (int i = 0; i < n; ++i) {
+            ddot += dx[i + _dx_offset] * dy[i + _dy_offset];
         }
         return ddot;
     }
