e8aa3e477be0c3b60c23e5d5d3746e6c0f732001
HullBend <HullBend@users.noreply.github.com>
2018-12-25 06:04:47 +0100

    Try to improve dtrmm performance

--- src/main/java/org/netlib/blas/Dtrmm.java
+++ src/main/java/org/netlib/blas/Dtrmm.java
@@ -9,6 +9,142 @@ import org.netlib.err.Xerbla;
 // op( A ) = A  or  op( A ) = A**T.
 public final class Dtrmm {
 
+    /**
+     * <pre><code>
+     *  Purpose
+     *  =======
+     *
+     *  DTRMM  performs one of the matrix-matrix operations
+     *
+     *     B := alpha*op( A )*B,   or   B := alpha*B*op( A ),
+     *
+     *  where  alpha  is a scalar,  B  is an m by n matrix,  A  is a unit, or
+     *  non-unit,  upper or lower triangular matrix  and  op( A )  is one  of
+     *
+     *     op( A ) = A   or   op( A ) = A'.
+     *
+     *  Arguments
+     *  ==========
+     *
+     *  SIDE   - CHARACTER*1.
+     *           On entry,  SIDE specifies whether  op( A ) multiplies B from
+     *           the left or right as follows:
+     *
+     *              SIDE = 'L' or 'l'   B := alpha*op( A )*B.
+     *
+     *              SIDE = 'R' or 'r'   B := alpha*B*op( A ).
+     *
+     *           Unchanged on exit.
+     *
+     *  UPLO   - CHARACTER*1.
+     *           On entry, UPLO specifies whether the matrix A is an upper or
+     *           lower triangular matrix as follows:
+     *
+     *              UPLO = 'U' or 'u'   A is an upper triangular matrix.
+     *
+     *              UPLO = 'L' or 'l'   A is a lower triangular matrix.
+     *
+     *           Unchanged on exit.
+     *
+     *  TRANSA - CHARACTER*1.
+     *           On entry, TRANSA specifies the form of op( A ) to be used in
+     *           the matrix multiplication as follows:
+     *
+     *              TRANSA = 'N' or 'n'   op( A ) = A.
+     *
+     *              TRANSA = 'T' or 't'   op( A ) = A'.
+     *
+     *              TRANSA = 'C' or 'c'   op( A ) = A'.
+     *
+     *           Unchanged on exit.
+     *
+     *  DIAG   - CHARACTER*1.
+     *           On entry, DIAG specifies whether or not A is unit triangular
+     *           as follows:
+     *
+     *              DIAG = 'U' or 'u'   A is assumed to be unit triangular.
+     *
+     *              DIAG = 'N' or 'n'   A is not assumed to be unit
+     *                                  triangular.
+     *
+     *           Unchanged on exit.
+     *
+     *  M      - INTEGER.
+     *           On entry, M specifies the number of rows of B. M must be at
+     *           least zero.
+     *           Unchanged on exit.
+     *
+     *  N      - INTEGER.
+     *           On entry, N specifies the number of columns of B.  N must be
+     *           at least zero.
+     *           Unchanged on exit.
+     *
+     *  ALPHA  - DOUBLE PRECISION.
+     *           On entry,  ALPHA specifies the scalar  alpha. When  alpha is
+     *           zero then  A is not referenced and  B need not be set before
+     *           entry.
+     *           Unchanged on exit.
+     *
+     *  A      - DOUBLE PRECISION array of DIMENSION ( LDA, k ), where k is m
+     *           when  SIDE = 'L' or 'l'  and is  n  when  SIDE = 'R' or 'r'.
+     *           Before entry  with  UPLO = 'U' or 'u',  the  leading  k by k
+     *           upper triangular part of the array  A must contain the upper
+     *           triangular matrix  and the strictly lower triangular part of
+     *           A is not referenced.
+     *           Before entry  with  UPLO = 'L' or 'l',  the  leading  k by k
+     *           lower triangular part of the array  A must contain the lower
+     *           triangular matrix  and the strictly upper triangular part of
+     *           A is not referenced.
+     *           Note that when  DIAG = 'U' or 'u',  the diagonal elements of
+     *           A  are not referenced either,  but are assumed to be  unity.
+     *           Unchanged on exit.
+     *
+     *  LDA    - INTEGER.
+     *           On entry, LDA specifies the first dimension of A as declared
+     *           in the calling (sub) program.  When  SIDE = 'L' or 'l'  then
+     *           LDA  must be at least  max( 1, m ),  when  SIDE = 'R' or 'r'
+     *           then LDA must be at least max( 1, n ).
+     *           Unchanged on exit.
+     *
+     *  B      - DOUBLE PRECISION array of DIMENSION ( LDB, n ).
+     *           Before entry,  the leading  m by n part of the array  B must
+     *           contain the matrix  B,  and  on exit  is overwritten  by the
+     *           transformed matrix.
+     *
+     *  LDB    - INTEGER.
+     *           On entry, LDB specifies the first dimension of B as declared
+     *           in  the  calling  (sub)  program.   LDB  must  be  at  least
+     *           max( 1, m ).
+     *           Unchanged on exit.
+     *
+     *
+     *  Level 3 Blas routine.
+     *
+     *  -- Written on 8-February-1989.
+     *     Jack Dongarra, Argonne National Laboratory.
+     *     Iain Duff, AERE Harwell.
+     *     Jeremy Du Croz, Numerical Algorithms Group Ltd.
+     *     Sven Hammarling, Numerical Algorithms Group Ltd.
+     *
+     *
+     *     .. External Functions ..
+     * </code>
+     * </pre>
+     *
+     * @param side
+     * @param uplo
+     * @param transa
+     * @param diag
+     * @param m
+     * @param n
+     * @param alpha
+     * @param a
+     * @param _a_offset
+     * @param lda
+     * @param b
+     * @param _b_offset
+     * @param ldb
+     */
     public static void dtrmm(String side, String uplo, String transa, String diag, int m, int n, double alpha,
             double[] a, int _a_offset, int lda, double[] b, int _b_offset, int ldb) {
 
@@ -34,271 +170,24 @@ public final class Dtrmm {
         } else if (ldb < Math.max(1, m)) {
             info = 11;
         }
+
         if (info != 0) {
             Xerbla.xerbla("DTRMM ", info);
             return;
         }
+
         // Quick return if possible
         if (n == 0) {
             return;
         }
-        // Quick return when == alpha zero
-        if (alpha == 0.0) {
-            int j = 1;
-            for (int p = n; p > 0; p--) {
-                int i = 1;
-                for (int q = m; q > 0; q--) {
-                    b[i - 1 + (j - 1) * ldb + _b_offset] = 0.0;
-                    i++;
-                }
 
-                j++;
-            }
-
-            return;
-        }
-        // Start the operations
-        if (lside) {
-            if (notrans) {
-                if (upper) {
-                    // lside, notrans, upper
-                    int j = 1;
-                    for (int p = n; p > 0; p--) {
-                        int k = 1;
-                        for (int q = m; q > 0; q--) {
-                            if (b[k - 1 + (j - 1) * ldb + _b_offset] != 0.0) {
-                                double temp = alpha * b[k - 1 + (j - 1) * ldb + _b_offset];
-                                int i = 1;
-                                for (int r = k - 1; r > 0; r--) {
-                                    b[i - 1 + (j - 1) * ldb + _b_offset] = b[i - 1 + (j - 1) * ldb + _b_offset]
-                                            + temp * a[i - 1 + (k - 1) * lda + _a_offset];
-                                    i++;
-                                }
-
-                                if (nounit) {
-                                    temp *= a[k - 1 + (k - 1) * lda + _a_offset];
-                                }
-                                b[k - 1 + (j - 1) * ldb + _b_offset] = temp;
-                            }
-                            k++;
-                        }
-
-                        j++;
-                    }
-
-                } else {
-                    // lside, notrans, !upper
-                    int j = 1;
-                    for (int p = n; p > 0; p--) {
-                        int k = m;
-                        for (int q = m; q > 0; q--) {
-                            if (b[k - 1 + (j - 1) * ldb + _b_offset] != 0.0) {
-                                double temp = alpha * b[k - 1 + (j - 1) * ldb + _b_offset];
-                                b[k - 1 + (j - 1) * ldb + _b_offset] = temp;
-                                if (nounit) {
-                                    b[k - 1 + (j - 1) * ldb + _b_offset] = b[k - 1 + (j - 1) * ldb + _b_offset]
-                                            * a[k - 1 + (k - 1) * lda + _a_offset];
-                                }
-                                int i = k + 1;
-                                for (int r = m - k; r > 0; r--) {
-                                    b[i - 1 + (j - 1) * ldb + _b_offset] = b[i - 1 + (j - 1) * ldb + _b_offset]
-                                            + temp * a[i - 1 + (k - 1) * lda + _a_offset];
-                                    i++;
-                                }
-
-                            }
-                            k--;
-                        }
-
-                        j++;
-                    }
-
-                }
-            } else if (upper) {
-                // Form B := alpha*A**T*B
-                // lside, !notrans, upper
-                int j = 1;
-                for (int p = n; p > 0; p--) {
-                    int i = m;
-                    for (int q = m; q > 0; q--) {
-                        double temp = b[i - 1 + (j - 1) * ldb + _b_offset];
-                        if (nounit) {
-                            temp *= a[i - 1 + (i - 1) * lda + _a_offset];
-                        }
-                        int k = 1;
-                        for (int r = i - 1; r > 0; r--) {
-                            temp += a[k - 1 + (i - 1) * lda + _a_offset] * b[k - 1 + (j - 1) * ldb + _b_offset];
-                            k++;
-                        }
-
-                        b[i - 1 + (j - 1) * ldb + _b_offset] = alpha * temp;
-                        i--;
-                    }
-
-                    j++;
-                }
-
-            } else {
-                // lside, !notrans, !upper
-                int j = 1;
-                for (int p = n; p > 0; p--) {
-                    int i = 1;
-                    for (int q = m; q > 0; q--) {
-                        double temp = b[i - 1 + (j - 1) * ldb + _b_offset];
-                        if (nounit) {
-                            temp *= a[i - 1 + (i - 1) * lda + _a_offset];
-                        }
-                        int k = i + 1;
-                        for (int r = m - i; r > 0; r--) {
-                            temp += a[k - 1 + (i - 1) * lda + _a_offset] * b[k - 1 + (j - 1) * ldb + _b_offset];
-                            k++;
-                        }
-
-                        b[i - 1 + (j - 1) * ldb + _b_offset] = alpha * temp;
-                        i++;
-                    }
-
-                    j++;
-                }
-
-            }
-        } else if (notrans) {
-            // Form B := alpha*B*A
-            // !lside, notrans
-            if (upper) {
-                // !lside, notrans, upper
-                int j = n;
-                for (int p = n; p > 0; p--) {
-                    double temp = alpha;
-                    if (nounit) {
-                        temp *= a[j - 1 + (j - 1) * lda + _a_offset];
-                    }
-                    int i = 1;
-                    for (int q = m; q > 0; q--) {
-                        b[i - 1 + (j - 1) * ldb + _b_offset] = temp * b[i - 1 + (j - 1) * ldb + _b_offset];
-                        i++;
-                    }
-
-                    int k = 1;
-                    for (int r = j - 1; r > 0; r--) {
-                        if (a[k - 1 + (j - 1) * lda + _a_offset] != 0.0) {
-                            temp = alpha * a[k - 1 + (j - 1) * lda + _a_offset];
-                            int ii = 1;
-                            for (int s = m; s > 0; s--) {
-                                b[ii - 1 + (j - 1) * ldb + _b_offset] = b[(ii - 1) + (j - 1) * ldb + _b_offset]
-                                        + temp * b[ii - 1 + (k - 1) * ldb + _b_offset];
-                                ii++;
-                            }
-
-                        }
-                        k++;
-                    }
-
-                    j--;
-                }
-
-            } else {
-                // !lside, notrans, !upper
-                int j = 1;
-                for (int p = n; p > 0; p--) {
-                    double temp = alpha;
-                    if (nounit) {
-                        temp *= a[j - 1 + (j - 1) * lda + _a_offset];
-                    }
-                    int i = 1;
-                    for (int q = m; q > 0; q--) {
-                        b[i - 1 + (j - 1) * ldb + _b_offset] = temp * b[i - 1 + (j - 1) * ldb + _b_offset];
-                        i++;
-                    }
-
-                    int k = j + 1;
-                    for (int r = n - j; r > 0; r--) {
-                        if (a[k - 1 + (j - 1) * lda + _a_offset] != 0.0) {
-                            temp = alpha * a[k - 1 + (j - 1) * lda + _a_offset];
-                            int ii = 1;
-                            for (int s = m; s > 0; s--) {
-                                b[ii - 1 + (j - 1) * ldb + _b_offset] = b[ii - 1 + (j - 1) * ldb + _b_offset]
-                                        + temp * b[ii - 1 + (k - 1) * ldb + _b_offset];
-                                ii++;
-                            }
-
-                        }
-                        k++;
-                    }
-
-                    j++;
-                }
-
-            }
-        } else if (upper) {
-            // Form B := alpha*B*A**T
-            // !lside, !notrans, upper
-            int k = 1;
-            for (int p = n; p > 0; p--) {
-                int j = 1;
-                for (int q = k - 1; q > 0; q--) {
-                    if (a[j - 1 + (k - 1) * lda + _a_offset] != 0.0) {
-                        double temp = alpha * a[j - 1 + (k - 1) * lda + _a_offset];
-                        int i = 1;
-                        for (int r = m; r > 0; r--) {
-                            b[i - 1 + (j - 1) * ldb + _b_offset] = b[i - 1 + (j - 1) * ldb + _b_offset]
-                                    + temp * b[i - 1 + (k - 1) * ldb + _b_offset];
-                            i++;
-                        }
-
-                    }
-                    j++;
-                }
-
-                double temp = alpha;
-                if (nounit) {
-                    temp *= a[k - 1 + (k - 1) * lda + _a_offset];
-                }
-                if (temp != 1.0) {
-                    int i = 1;
-                    for (int s = m; s > 0; s--) {
-                        b[i - 1 + (k - 1) * ldb + _b_offset] = temp * b[i - 1 + (k - 1) * ldb + _b_offset];
-                        i++;
-                    }
-
-                }
-                k++;
-            }
-
+        if ((lside && Math.round(Math.sqrt(m * n)) > 190L) || (!lside && Math.round(Math.sqrt(m * n)) > 280L)) {
+            // FIXME
+            System.err.println("Using Ulm TRMM with m = " + m + ", n = " + n);
+            // FIXME
+            Trmm.trmm(lside, !upper, !notrans, !nounit, m, n, alpha, a, _a_offset, lda, b, _b_offset, ldb);
         } else {
-            // !lside, !notrans, !upper
-            int k = n;
-            for (int p = n; p > 0; p--) {
-                int j = k + 1;
-                for (int q = n - k; q > 0; q--) {
-                    if (a[j - 1 + (k - 1) * lda + _a_offset] != 0.0) {
-                        double temp = alpha * a[j - 1 + (k - 1) * lda + _a_offset];
-                        int i = 1;
-                        for (int r = m; r > 0; r--) {
-                            b[i - 1 + (j - 1) * ldb + _b_offset] = b[i - 1 + (j - 1) * ldb + _b_offset]
-                                    + temp * b[i - 1 + (k - 1) * ldb + _b_offset];
-                            i++;
-                        }
-
-                    }
-                    j++;
-                }
-
-                double temp = alpha;
-                if (nounit) {
-                    temp *= a[k - 1 + (k - 1) * lda + _a_offset];
-                }
-                if (temp != 1.0) {
-                    int i = 1;
-                    for (int s = m; s > 0; s--) {
-                        b[i - 1 + (k - 1) * ldb + _b_offset] = temp * b[i - 1 + (k - 1) * ldb + _b_offset];
-                        i++;
-                    }
-
-                }
-                k--;
-            }
-
+            DtrmmNetlib.dtrmm(lside, upper, notrans, nounit, m, n, alpha, a, _a_offset, lda, b, _b_offset, ldb);
         }
     }
 }
